70c5bc3db523e46848adf8bf6121c53dbb80327f
Milos Cibulka <mcibique@users.noreply.github.com>
2019-10-29 15:02:02 +0000

    ONL-3814: chore: Renamed EcContainer to EcMainContainer.

---
+++ src/components/ec-main-container/ec-main-container.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="ec-main-container">
+    <template v-if="title">
+      <h1 class="ec-main-container__title">{{ title }}</h1>
+      <p class="ec-main-container__title-intro">{{ titleIntro }}</p>
+    </template>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EcMainContainer',
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    titleIntro: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../scss/tools/typography';
+@import '../../scss/settings/colors/gray-color-scale';
+
+.ec-main-container {
+  padding: 24px;
+  min-height: 100%;
+
+  &__title {
+    @include h1;
+
+    margin-top: 0;
+    margin-bottom: 8px;
+  }
+
+  &__title-intro {
+    @include body-text;
+
+    color: $level-4-interactive-elements;
+    margin-top: 0;
+    margin-bottom: 16px;
+  }
+}
+</style>
