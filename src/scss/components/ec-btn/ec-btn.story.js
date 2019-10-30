f7241945b0258737cd626709be6b37c0d4b6c167
stavros <stavros.tomadakis@ebury.com>
2019-10-30 15:23:54 +0000

    ONL-3933 fix: code review comments

--- src/scss/components/ec-btn/ec-btn.story.js
+++ src/scss/components/ec-btn/ec-btn.story.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/vue';
-import { text, boolean } from '@storybook/addon-knobs';
+import { boolean } from '@storybook/addon-knobs';
 import StoryRouter from 'storybook-vue-router';
 import EcIcon from '@/components/ec-icon';
 
@@ -11,9 +11,6 @@ stories
   .add('basic', () => ({
     components: { EcIcon },
     props: {
-      btnclass: {
-        default: text('class', 'ec-btn ec-btn--primary'),
-      },
       isDisabled: {
         default: boolean('Disabled', false),
       },
@@ -21,7 +18,7 @@ stories
         default: boolean('Has Icon', false),
       },
       isSmall: {
-        default: boolean('ec-btn--sm', false),
+        default: boolean('ec-btn--sm', true),
       },
       isMedium: {
         default: boolean('ec-btn--md', false),
@@ -45,72 +42,59 @@ stories
         default: boolean('ec-btn--error', false),
       },
     },
+    computed: {
+      classNames() {
+        return {
+          'ec-btn--primary': this.isPrimary,
+          'ec-btn--secondary': this.isSecondary,
+          'ec-btn--success': this.isSuccess,
+          'ec-btn--error': this.isError,
+          'ec-btn--rounded': this.isRounded,
+          'ec-btn--negative': this.isNegative,
+          'ec-btn--sm': this.isSmall,
+          'ec-btn--md': this.isMedium,
+        };
+      },
+    },
     template: `
       <div style="margin:20px;">
         <div>
-          <h3 class="ec--m10">This is a button tag with a "ec-btn" class</h3>
+          <h3 class="ec-m--8">This is a button tag with a "ec-btn" class</h3>
           <button
-            :class="{
-              'ec-btn--primary' : isPrimary,
-              'ec-btn--secondary' : isSecondary,
-              'ec-btn--success' : isSuccess,
-              'ec-btn--error' : isError,
-              'ec-btn--rounded' : isRounded,
-              'ec-btn--negative' : isNegative,
-              'ec-btn--sm' : isSmall,
-              'ec-btn--md' : isMedium,
-            }"
-            class="ec--m10 ec-btn"
+            :class="classNames"
+            class="ec-m--8 ec-btn"
             :disabled="isDisabled"
             >
-            <ec-icon v-if="hasIcon" name="simple-check" style="margin-right:10px;" size="20"/>
+            <ec-icon v-if="hasIcon" name="simple-check" style="margin-right:10px;" :size=20 />
             Click Me
           </button>
-        <div>
+        </div>
 
         <div>
-          <h3 class="ec--m10">This is an a tag with a "ec-btn" class</h3>
+          <h3 class="ec-m--8">This is an a tag with a "ec-btn" class</h3>
           <a
             href="https://online.ebury.com/login/?next=/"
-            :class="{
-              'ec-btn--primary' : isPrimary,
-              'ec-btn--secondary' : isSecondary,
-              'ec-btn--success' : isSuccess,
-              'ec-btn--error' : isError,
-              'ec-btn--rounded' : isRounded,
-              'ec-btn--negative' : isNegative,
-              'ec-btn--sm' : isSmall,
-              'ec-btn--md' : isMedium,
-            }"
-            class="ec--m10 ec-btn"
+            :class="classNames"
+            class="ec-m--8 ec-btn"
             :disabled="isDisabled"
             >
-            <ec-icon v-if="hasIcon" name="simple-check" style="margin-right:10px;" size="20"/>
+            <ec-icon v-if="hasIcon" name="simple-check" style="margin-right:10px;" :size=20 />
             Click Me
           </a>
         </div>
 
         <div>
-          <h3 class="ec--m10">This is a router-link tag with a "ec-btn" class</h3>
+          <h3 class="ec-m--8">This is a router-link tag with a "ec-btn" class</h3>
           <router-link
             to="/someurl"
-            :class="{
-              'ec-btn--primary' : isPrimary,
-              'ec-btn--secondary' : isSecondary,
-              'ec-btn--success' : isSuccess,
-              'ec-btn--error' : isError,
-              'ec-btn--rounded' : isRounded,
-              'ec-btn--negative' : isNegative,
-              'ec-btn--sm' : isSmall,
-              'ec-btn--md' : isMedium,
-            }"
-            class="ec--m10 ec-btn"
+            :class="classNames"
+            class="ec-m--8 ec-btn"
             :disabled="isDisabled"
             >
-            <ec-icon v-if="hasIcon" name="simple-check" style="margin-right:10px;" size="20"/>
+            <ec-icon v-if="hasIcon" name="simple-check" style="margin-right:10px;" :size=20 />
             Click Me
           </router-link>
-        <div>
+        </div>
       </div>
     `,
   }))
@@ -118,46 +102,46 @@ stories
     template: `
       <div style="margin:20px;">
 
-        <h3 class="ec--m10">Solid Buttons</h3>
-        <button class="ec-btn ec-btn--md ec-btn--primary ec--m10">Primary</button>
-        <button class="ec-btn ec-btn--md ec-btn--secondary ec--m10">Secondary</button>
-        <button class="ec-btn ec-btn--sm ec-btn--success ec--m10">Success</button>
-        <button class="ec-btn ec-btn--sm ec-btn--error ec--m10">Error</button>
-        <button class="ec-btn ec-btn--sm ec--m10" disabled>Disabled</button>
+        <h3 class="ec-m--8">Solid Buttons</h3>
+        <button class="ec-btn ec-btn--md ec-btn--primary ec-m--8">Primary</button>
+        <button class="ec-btn ec-btn--md ec-btn--secondary ec-m--8">Secondary</button>
+        <button class="ec-btn ec-btn--sm ec-btn--success ec-m--8">Success</button>
+        <button class="ec-btn ec-btn--sm ec-btn--error ec-m--8">Error</button>
+        <button class="ec-btn ec-btn--sm ec-m--8" disabled>Disabled</button>
 
-        <h3 class="ec--m10">Solid Rounded Buttons</h3>
-        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--primary ec--m10">Primary</button>
-        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--secondary ec--m10">Secondary</button>
-        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--success ec--m10">Success</button>
-        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--error ec--m10">Error</button>
-        <button class="ec-btn ec-btn--sm ec-btn--rounded ec--m10" disabled>Disabled</button>
+        <h3 class="ec-m--8">Solid Rounded Buttons</h3>
+        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--primary ec-m--8">Primary</button>
+        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--secondary ec-m--8">Secondary</button>
+        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--success ec-m--8">Success</button>
+        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--error ec-m--8">Error</button>
+        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-m--8" disabled>Disabled</button>
 
-        <h3 class="ec--m10">Outline Buttons</h3>
-        <button class="ec-btn ec-btn--md ec-btn--outline ec-btn--primary ec--m10">Primary</button>
-        <button class="ec-btn ec-btn--md ec-btn--outline ec-btn--secondary ec--m10">Secondary</button>
-        <button class="ec-btn ec-btn--sm ec-btn--outline ec-btn--success ec--m10">Success</button>
-        <button class="ec-btn ec-btn--sm ec-btn--outline ec-btn--error ec--m10">Error</button>
-        <button class="ec-btn ec-btn--sm ec--m10 ec-btn--outline" disabled>Disabled</button>
+        <h3 class="ec-m--8">Outline Buttons</h3>
+        <button class="ec-btn ec-btn--md ec-btn--outline ec-btn--primary ec-m--8">Primary</button>
+        <button class="ec-btn ec-btn--md ec-btn--outline ec-btn--secondary ec-m--8">Secondary</button>
+        <button class="ec-btn ec-btn--sm ec-btn--outline ec-btn--success ec-m--8">Success</button>
+        <button class="ec-btn ec-btn--sm ec-btn--outline ec-btn--error ec-m--8">Error</button>
+        <button class="ec-btn ec-btn--sm ec-m--8 ec-btn--outline" disabled>Disabled</button>
         
-        <h3 class="ec--m10">Outline Rounded Buttons</h3>
-        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--outline ec-btn--primary ec--m10">Primary</button>
-        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--outline ec-btn--secondary ec--m10">Secondary</button>
-        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--outline ec-btn--success ec--m10">Success</button>
-        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--outline ec-btn--error ec--m10">Error</button>
-        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--outline ec--m10" disabled>Disabled</button>
+        <h3 class="ec-m--8">Outline Rounded Buttons</h3>
+        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--outline ec-btn--primary ec-m--8">Primary</button>
+        <button class="ec-btn ec-btn--md ec-btn--rounded ec-btn--outline ec-btn--secondary ec-m--8">Secondary</button>
+        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--outline ec-btn--success ec-m--8">Success</button>
+        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--outline ec-btn--error ec-m--8">Error</button>
+        <button class="ec-btn ec-btn--sm ec-btn--rounded ec-btn--outline ec-m--8" disabled>Disabled</button>
       </div>
     `,
   }))
   .add('ghost', () => ({
     template: `
       <div style="padding:20px; background:#005166;width:100vw;height:100vh;">
-        <h3 class="ec--m10" style="color:white;" >Ghost Buttons</h3>
-        <button class="ec-btn ec-btn--negative ec--m10">Primary</button>
-        <button class="ec-btn ec-btn--negative ec-btn--primary ec-btn--rounded ec--m10">Primary</button>
+        <h3 class="ec-m--8" style="color:white;" >Ghost Buttons</h3>
+        <button class="ec-btn ec-btn--negative ec-m--8">Primary</button>
+        <button class="ec-btn ec-btn--negative ec-btn--primary ec-btn--rounded ec-m--8">Primary</button>
         
-        <h3 class="ec--m10" style="color:white;" >Disabled Ghost Buttons</h3>
-        <button class="ec-btn ec-btn--negative ec--m10" disabled>Disabled</button>
-        <button class="ec-btn ec-btn--negative ec-btn--rounded ec--m10" disabled>Disabled</button>
+        <h3 class="ec-m--8" style="color:white;" >Disabled Ghost Buttons</h3>
+        <button class="ec-btn ec-btn--negative ec-m--8" disabled>Disabled</button>
+        <button class="ec-btn ec-btn--negative ec-btn--rounded ec-m--8" disabled>Disabled</button>
       </div>
     `,
   }));
