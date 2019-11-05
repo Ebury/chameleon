5dbc6b3475d218fb405cfb133558dfdaa0b4a30d
stavros <stavros.tomadakis@ebury.com>
2019-11-05 11:40:51 +0000

    ONL-3848 fix: remove navigation story

--- src/components/ec-user-info/ec-user-info.story.js
+++ src/components/ec-user-info/ec-user-info.story.js
@@ -1,7 +1,6 @@
 import { storiesOf } from '@storybook/vue';
 import { object, boolean } from '@storybook/addon-knobs';
 import EcUserInfo from './ec-user-info.vue';
-import EcNavigation from '@/components/ec-navigation/ec-navigation.vue';
 
 const client = {
   name: 'Ebury Demo 2',
@@ -42,82 +41,6 @@ stories.add('basic', () => ({
       </ ec-user-info>
     </div>
   `,
-}))
-  .add('with navigation', () => ({
-    components: {
-      EcNavigation,
-      EcUserInfo,
-    },
-    props: {
-      client: {
-        default: object('user', client),
-      },
-      branding: {
-        default: object('branding', {
-          name: 'My Branding',
-          logo: 'http://ebo.localhost/static/img/branding/EBP/logo-sm-inverse.png',
-        }),
-      },
-      isCollapsable: {
-        default: boolean('isCollapsable', false),
-      },
-      isCollapsed: {
-        default: boolean('isCollapsed', false),
-      },
-      showBrandingLogo: {
-        default: boolean('showBrandingLogo', true),
-      },
-      showCopyright: {
-        default: boolean('showCopyright', true),
-      },
-    },
-    data() {
-      return {
-        styles: {
-          placeholder: {
-            backgroundColor: 'rgba(255, 255, 255, .1)',
-            padding: '8px',
-            textAlign: 'center',
-            height: '100%',
-            wordBreak: 'break-word',
-          },
-        },
-      };
-    },
-    template: `
-    <ec-navigation
-      :branding="branding"
-      :show-branding-logo="showBrandingLogo"
-      :is-collapsable="isCollapsable"
-      :is-collapsed="isCollapsable && isCollapsed">
-      <template #user-info>
-      <ec-user-info
-        :user="client"
-        :is-collapsable="isCollapsable"
-        :is-collapsed="isCollapsable && isCollapsed"
-      >
-        <template v-slot:dropdown-search>
-          <select name="pets" id="pet-select">
-            <option value="ebury">Ebury</option>
-            <option value="eburydemo2">EburyDemo2</option>
-          </select>
-        </template>
-      </ ec-user-info>
-      </template>
-      <template #call-to-action>
-        <div :style="styles.placeholder">CTA placeholder</div>
-      </template>
-      <template #menu>
-        <div :style="styles.placeholder">Menu placeholder</div>
-      </template>
-      <template #footer-menu>
-        <div :style="styles.placeholder">Footer Menu placeholder</div>
-      </template>
-      <template #copyright v-if="showCopyright">
-        <div :style="styles.placeholder">Copyright placeholder</div>
-      </template>
-    </ec-navigation>`,
-  }));
-
+}));
 
 export default stories;
