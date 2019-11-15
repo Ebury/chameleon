fd56b8225327e3669e152e735512c979005eb642
jaimeperezpedra <jaimeperezpedra@gmail.com>
2019-11-15 11:15:08 +0000

    ONL-3958 WIP

--- src/components/ec-privacy-policy/ec-privacy-policy.story.js
+++ src/components/ec-privacy-policy/ec-privacy-policy.story.js
@@ -7,7 +7,9 @@ const stories = storiesOf('Privacy Policy', module);
 stories.add('basic', () => ({
   components: { EcPrivacyPolicy },
   template: `
-    <ec-privacy-policy></ec-privacy-policy>
+    <ec-privacy-policy button-text="Accept and continue" title="Cookies and Privacy">
+      This site uses cookies to ensure you get the best experience. For more information see our <a>Privacy Policy</a>
+    </ec-privacy-policy>
   `,
 }));
 
--- src/components/ec-privacy-policy/ec-privacy-policy.vue
+++ src/components/ec-privacy-policy/ec-privacy-policy.vue
@@ -1,15 +1,70 @@
 <template>
-  <div class="ec-privacy-policy">asd</div>
+  <div class="ec-privacy-policy">
+    <div class="ec-privacy-policy__content">
+      <div class="ec-privacy-policy__title ec-privacy-policy__item">{{ title }}</div>
+      <div class="ec-privacy-policy__item">
+        <slot />
+      </div>
+      <div class="ec-privacy-policy__item">
+        <button
+          class="ec-privacy-policy__item ec-btn ec-btn--rounded ec-btn-secondary ec-btn--sm"
+          @click="$emit('accept')"
+        >{{ buttonText }}</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'EcPrivacyPolicy',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+@import '../../scss/settings/z-index';
+@import '../../scss/tools/typography';
+@import '../../scss/tools/media-queries';
+
 .ec-privacy-policy {
-  background-color: red;
+  z-index: $z-index-level-3;
+  padding: 24px;
+  background-color: #ebfbff;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  opacity: 0.95;
+
+  &__content {
+    max-width: 1184px;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    align-content: center;
+    justify-content: space-between;
+  }
+
+  &__title {
+    @include h4;
+  }
+
+  &__item {
+    @media screen and (max-width: 1184px) {
+      width: 100%;
+      margin: 0.25rem auto;
+    }
+  }
 }
 </style>
