b0fd8b4329a690724af281d38cd0f70e5dafe8a6
jaimeperezpedra <jaime.perez@ebury.com>
2019-11-18 10:05:23 +0000

    Onl 3958 privacy policy component (#23)
    * ONL-3958 WIP
    
    * ONL-3958 write unit test
    
    * ONL-3958 remove commented code
    
    * 0.1.15

---
+++ src/components/ec-privacy-policy/ec-privacy-policy.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="ec-privacy-policy">
+    <div class="ec-privacy-policy__content">
+      <div class="ec-privacy-policy__title ec-privacy-policy__item">{{ title }}</div>
+      <div class="ec-privacy-policy__item">
+        <slot />
+      </div>
+      <div class="ec-privacy-policy__item">
+        <button
+          class="ec-privacy-policy__btn ec-btn ec-btn--rounded ec-btn-secondary ec-btn--sm"
+          @click="$emit('accept')"
+        >{{ buttonText }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EcPrivacyPolicy',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../scss/settings/colors/key-color-scale';
+@import '../../scss/settings/layout';
+@import '../../scss/settings/z-index';
+@import '../../scss/tools/typography';
+@import '../../scss/tools/media-queries';
+
+$ec-privacy-policy-max-width: $body-max-width !default;
+
+.ec-privacy-policy {
+  z-index: $z-index-level-1;
+  padding: 24px 48px;
+  background-color: rgba($level-7, 0.95);
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+
+  @media screen and (max-width: 640px) {
+    padding: 24px 16px;
+  }
+
+  &__content {
+    max-width: $ec-privacy-policy-max-width;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__title {
+    @include h4;
+  }
+
+  &__item {
+    @media screen and (max-width: $ec-privacy-policy-max-width) {
+      width: 100%;
+      margin: 4px auto;
+    }
+  }
+}
+</style>
