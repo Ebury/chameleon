ac9fa0b902816d004919c08a724a1d1405b459aa
stavros <stavros.tomadakis@ebury.com>
2019-11-20 16:50:14 +0000

    basic toaster in place

--- src/components/ec-toaster/ec-toaster.story.js
+++ src/components/ec-toaster/ec-toaster.story.js
@@ -1,13 +1,96 @@
 import { storiesOf } from '@storybook/vue';
+import { text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 import EcToaster from './ec-toaster.vue';
 
 const stories = storiesOf('Toaster', module);
 
 stories.add('basic', () => ({
   components: { EcToaster },
-  template: `
-    <EcToaster>
-    `,
+  data() {
+    return {
+      show: true,
+      messages: [
+        {
+          id: 1,
+          type: 'success',
+          title: 'This is the title',
+          subtitle: 'This is the subtitle',
+          dismissable: true,
+        },
+        {
+          id: 2,
+          type: 'error',
+          title: 'This is the title',
+          subtitle: 'This is the subtitffffffle',
+          dismissable: true,
+        },
+        {
+          id: 3,
+          type: 'success',
+          title: 'This is the title',
+          subtitle: 'This is the subtitle',
+          dismissable: true,
+        },
+        {
+          id: 4,
+          type: 'error',
+          title: 'This is the title',
+          subtitle: 'This is the subtitle',
+          dismissable: true,
+        },
+        {
+          id: 5,
+          type: 'error',
+          title: 'This is the title',
+          subtitle: 'This is the subtitlffffffffffffffffffe',
+          dismissable: true,
+        },
+      ],
+    };
+  },
+  props: {
+    type: {
+      default: text('Type', 'success'),
+    },
+    title: {
+      default: text('Title', 'A new title'),
+    },
+    subtitle: {
+      default: text('Subtitle', 'A new subtitle'),
+    },
+  },
+  methods: {
+    removeMessage(message) {
+      action('User closed the message')();
+      this.messages = this.messages.filter(m => m !== message);
+    },
+    addMessage() {
+      this.messages.push({
+        id: +new Date(),
+        type: this.type,
+        title: this.title,
+        subtitle: this.subtitle,
+        dismissable: true,
+      });
+    },
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  render(h) {
+    return (
+      <div style={{
+        position: 'fixed',
+        right: 0,
+        top: 0,
+        width: '100%',
+        maxWidth: '400px',
+      }}>
+        <EcToaster messages={this.messages} onRemove={message => this.removeMessage(message)}/>
+        <button style="position:fixed;top:50%;left:50%;transform: translate(-50%, -50%);" class="ec-btn ec-btn--md ec-btn--rounded ec-btn--primary" onClick={this.addMessage}>Amend Props and Click </button>
+      </div>
+    );
+  },
 }));
 
 export default stories;
--- src/components/ec-toaster/ec-toaster.vue
+++ src/components/ec-toaster/ec-toaster.vue
@@ -1,15 +1,114 @@
 <template>
-  <div class="ec-toaster">Here goes the toast</div>
+  <div class="ec-toaster">
+    <transition-group
+      ref="list"
+      name="list"
+      tag="ul"
+      class="ec-toaster__list"
+      appear
+      @after-enter="rememberTopItemPositions"
+      @before-leave="rememberTopItemPositions"
+    >
+      <li
+        v-for="message of messages"
+        :key="message.id"
+        ref="items"
+        v-toaster-touch="{ minDistance: 50 }"
+        class="ec-toaster__item"
+        @toaster-touch-remove="$emit('remove', message)"
+      >
+        <ec-alert
+          :type="message.type"
+          :title="message.title"
+          :subtitle="message.subtitle"
+          :dismissable="message.dismissable"
+          @change="$emit('remove', message)"
+        />
+      </li>
+    </transition-group>
+  </div>
 </template>
 
 <script>
-export default {
+import EcAlert from '../ec-alert';
+import ToasterTouchDirective from '../../directives/ec-toaster-touch';
 
+export default {
+  name: 'Toaster',
+  components: {
+    EcAlert,
+  },
+  directives: { ToasterTouch: ToasterTouchDirective },
+  props: {
+    messages: { type: Array, default: () => [] },
+  },
+  methods: {
+    rememberTopItemPositions() {
+      if (this.$refs.items) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const item of this.$refs.items) {
+          item.style.top = `${item.offsetTop}px`;
+        }
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-  body {
-    background: rgb(114, 28, 28);
+.ec-toaster {
+  position: fixed;
+  z-index: 300;
+  top: 0;
+  right: 0;
+  left: auto;
+  pointer-events: none;
+  max-width: 400px;
+  width: 100%;
+
+  &__list {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    position: relative;
+    border: 1px solid tomato;
+    overflow: visible;
+    padding: 10px;
+    margin: 0;
+    pointer-events: none;
   }
+
+  &__item {
+    display: inline-block;
+    max-width: 100%;
+    flex-grow: 0;
+    flex-shrink: 0;
+    // padding: 8px;
+    // border: 1px solid #ccc;
+    transition-property: opacity, transform;
+    transition-duration: 1s;
+    margin-bottom: 8px;
+    pointer-events: auto;
+    // background-color: #fff;
+
+    &--swipe-active {
+      transition-duration: 0s; // do not transition while swiping, let the element to follow the finger.
+    }
+
+    &--removed-by-swipe {
+      transition-duration: 0.3s;
+    }
+  }
+}
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+
+.list-leave-active {
+  position: absolute;
+}
 </style>
