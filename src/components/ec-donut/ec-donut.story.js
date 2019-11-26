3f8717924e8dafd766558586afc27b1724922e80
jaimeperezpedra <jaime.perez@ebury.com>
2019-11-26 16:28:11 +0000

    ONL 4037 donut component (#36)
    * ONL-4037 create the donut component
    
    * ONL-4037 add slots
    
    * ONL-4037 make responsive the component

---
+++ src/components/ec-donut/ec-donut.story.js
@@ -0,0 +1,51 @@
+import { storiesOf } from '@storybook/vue';
+import { number, select } from '@storybook/addon-knobs';
+import EcDonut from './ec-donut.vue';
+
+const stories = storiesOf('Donut chart', module);
+
+stories.add('basic', () => ({
+  components: { EcDonut },
+  filters: {
+    currencyFormat(value, currency) {
+      return new Intl.NumberFormat('gb-GB', { style: 'currency', currency, currencyDisplay: 'code' }).format(value);
+    },
+  },
+  props: {
+    used: {
+      default: number('Used', 2500),
+    },
+    amount: {
+      default: number('Amount', 10000),
+    },
+    currency: {
+      default: select('Currency', ['GBP', 'EUR', 'USD', 'CAD'], 'GBP'),
+    },
+  },
+  computed: {
+    remaining() {
+      if (this.used > this.amount) {
+        return 0;
+      } if (this.used <= 0) {
+        return this.amount;
+      }
+      return this.amount - this.used;
+    },
+  },
+  template: `
+  <div style="display: flex; height: 100vh">
+    <div style="margin: auto" class="ec-card">
+      <div style="text-align: center;" class="ec-mb--24">Credit line: <strong>{{ amount | currencyFormat(currency) }}</strong></div>
+      <ec-donut class="ec-p--8" :used="used" :amount="amount">
+        <template #remaining-legend>
+          <span><strong>Remaining: </strong>{{ remaining | currencyFormat(currency) }}</span>
+        </template>
+        <template #used-legend>
+          <span><strong>Used: </strong>{{ used | currencyFormat(currency) }}</span>
+        </template>
+      </ec-donut>
+    </div>
+  </div>`,
+}));
+
+export default stories;
---
+++ src/components/ec-donut/ec-donut.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="ec-donut">
+    <div class="ec-donut__container">
+      <svg
+        width="108"
+        height="108"
+        viewBox="0 0 108 108"
+        class="ec-donut__svg"
+      >
+        <circle
+          class="ec-donut__background"
+          cx="54"
+          cy="54"
+          :r="radius"
+          fill="transparent"
+          stroke-width="11"
+        />
+        <circle
+          class="ec-donut__remaining"
+          cx="54"
+          cy="54"
+          :r="radius"
+          fill="transparent"
+          stroke-width="11"
+          :stroke-dasharray="dashArray"
+          :stroke-dashoffset="0"
+        />
+        <circle
+          class="ec-donut__used"
+          cx="54"
+          cy="54"
+          :r="radius"
+          fill="transparent"
+          stroke-width="11"
+          :stroke-dasharray="dashArray"
+          :stroke-dashoffset="dashOffset"
+        />
+      </svg>
+    </div>
+    <div class="ec-donut__legend">
+      <div class="ec-donut__legend-used">
+        <ec-icon
+          name="rounded-notification"
+          :size="14"
+          class="ec-donut__legend-icon ec-donut__legend-icon--used"
+        />
+        <slot name="used-legend" />
+      </div>
+      <div class="ec-donut__legend-remaining">
+        <ec-icon
+          name="rounded-notification"
+          :size="14"
+          class="ec-donut__legend-icon ec-donut__legend-icon--remaining"
+        />
+        <slot name="remaining-legend" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import EcIcon from '../ec-icon';
+
+export default {
+  name: 'EcDonut',
+  components: { EcIcon },
+  props: {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    used: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      radius: 48,
+    };
+  },
+  computed: {
+    percentageUsed() {
+      if (this.used > this.amount) {
+        return 100;
+      } if (this.used <= 0) {
+        return 0;
+      }
+      return (this.used / this.amount) * 100;
+    },
+    // Calculate the circumference
+    dashArray() {
+      return 2 * Math.PI * this.radius;
+    },
+    // Calculate the how much needs to be offset of the used circle
+    dashOffset() {
+      return this.dashArray * (1 - this.percentageUsed / 100);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../scss/settings/colors/index';
+@import '../../scss/tools/';
+
+.ec-donut {
+  display: flex;
+  flex-wrap: wrap;
+
+  &__container {
+    margin-right: 24px;
+    height: 108px;
+
+    @include media__up-to-sm {
+      width: 100%;
+      margin-right: 0;
+      display: flex;
+      justify-content: center;
+    }
+  }
+
+  &__legend {
+    @include h4;
+
+    line-height: 24px;
+    flex-wrap: wrap;
+    padding: 16px 0;
+    color: $level-3-body-and-headings;
+
+    @include media__up-to-sm {
+      padding: 16px 0 0;
+      display: inline-flex;
+      flex-direction: column;
+      margin: 0 auto;
+    }
+  }
+
+  &__legend-used {
+    margin-bottom: 24px;
+
+    @include media__up-to-sm {
+      margin-bottom: 12px;
+    }
+  }
+
+  &__legend-used,
+  &__legend-remaining {
+    display: flex;
+    align-items: center;
+  }
+
+  &__legend-icon {
+    flex-shrink: 0;
+    margin-right: 8px;
+
+    &--used {
+      fill: rgba($color-info, 0.5);
+    }
+
+    &--remaining {
+      fill: $color-info;
+    }
+  }
+
+  &__svg {
+    transform: rotate(-90deg);
+  }
+
+  &__background {
+    stroke: $level-7-backgrounds;
+  }
+
+  &__remaining {
+    stroke: $color-info;
+  }
+
+  &__used {
+    stroke: $white;
+    stroke-opacity: 0.5;
+    animation: progress 1s ease-out;
+  }
+}
+
+@keyframes progress {
+  0% {
+    // This number is the (2 * Math.PI * radius)
+    stroke-dashoffset: 301.59289474462014;
+  }
+}
+</style>
