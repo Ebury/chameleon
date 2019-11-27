8f2ed7ecf4a2bda2b4e562fd2a4819bf466a790f
stavros <stavros.tomadakis@ebury.com>
2019-11-27 19:43:34 +0000

    fix: refactored grid

--- src/scss/components/ec-grid/ec-grid.story.js
+++ src/scss/components/ec-grid/ec-grid.story.js
@@ -1,150 +1,174 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-plusplus */
 import { storiesOf } from '@storybook/vue';
-import './_ec-grid.story.scss';
+import './ec-grid.story.scss';
 
 const stories = storiesOf('Grid', module);
 
 stories
-  .add('basic', () => ({
+  .add('rows', () => ({
     template: `
     <div class="grid-story">
       <h1 class="grid-story__title">12 Columns - 24 pixel gutters</h1>
-      <div class="ec-grid ec-grid--row-sm grid-story__container">
-        <div
-          v-for="i in 12"
-          class="ec-col-1 grid-story__column">
-          <p>.ec-col-1</p>
+      <h2 class="grid-story__title">Grandparent: "ec-grid", Parent: "ec-grid__row" Child: "ec-col-1 to ec-12"</h2>
+      <h2 class="grid-story__title">Normal</h2>
+      <div class="ec-grid">
+        <div class="ec-grid__row grid-story__container">
+          <div
+            v-for="i in 12"
+            class="ec-col-1 grid-story__column">
+            <p>.ec-col-1 <div>{{ i }}</div></p>
+          </div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">.ec-grid</h1>
-      <div class="ec-grid ec-grid--row-sm grid-story__container">
-        <div
-          v-for="i in 6"
-          class="ec-col-2 grid-story__column">
-          <p>.ec-col-2</p>
+      <h1 class="grid-story__title">Reverse</h1>
+      <div class="ec-grid">
+        <div class="ec-grid__row--reverse grid-story__container">
+          <div
+            v-for="i in 12"
+            class="ec-col-1 grid-story__column">
+            <p>.ec-col-1 <div>{{ i }}</div></p>
+          </div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">.ec-grid</h1>
-      <div class="ec-grid ec-grid--row-sm grid-story__container">
-        <div
-          v-for="i in 4"
-          class="ec-col-3 grid-story__column">
-          <p>.ec-col-3</p>
+      <h1 class="grid-story__title">Wrap</h1>
+      <div class="ec-grid">
+        <div class="ec-grid__row ec-grid__row--wrap grid-story__container">
+          <div
+            v-for="i in 18"
+            class="ec-col-1 grid-story__column">
+            <p>.ec-col-1 <div>{{ i }}</div></p>
+          </div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">.ec-grid</h1>
-      <div class="ec-grid ec-grid--row-sm grid-story__container">
-        <div
-          v-for="i in 3"
-          class="ec-col-4 grid-story__column">
-          <p>.ec-col-4</p>
+      <h1 class="grid-story__title">Offset</h1>
+      <div class="ec-grid">
+        <div class="ec-grid__row grid-story__container">
+          <div class="ec-col-1 ec-offset-1 grid-story__column"><p>.ec-offset-1</p></div>
+          <div class="ec-col-1 ec-offset-2 grid-story__column"><p>.ec-offset-2</p></div>
+          <div class="ec-col-1 ec-offset-4 grid-story__column"><p>.ec-offset-4</p></div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">.ec-grid</h1>
-      <div class="ec-grid ec-grid--row-sm grid-story__container">
-        <div
-          v-for="i in 2"
-          class="ec-col-6 grid-story__column">
-          <p>.ec-col-6</p>
+      <h1 class="grid-story__title">Various column combinations</h1>
+      <div class="ec-grid">
+        <div class="ec-grid__row grid-story__container">
+          <div class="ec-col-2 grid-story__column"><p>.ec-col-2</p></div>
+          <div class="ec-col-4 grid-story__column"><p>.ec-col-4</p></div>
+          <div class="ec-col-6 grid-story__column"><p>.ec-col-6</p></div>
+        </div>
+      </div>
+    `,
+  }))
+  .add('columns', () => ({
+    template: `
+    <div class="grid-story">
+      <h1 class="grid-story__title">12 Columns - 24 pixel gutters</h1>
+      <h2 class="grid-story__title">Grandparent: "ec-grid", Parent: "ec-grid__col" Child: "ec-col-1 to ec-12"</h2>
+      <div class="ec-grid">
+        <div class="ec-grid__col grid-story__container">
+          <div
+            v-for="i in 12"
+            class="ec-col-1 grid-story__column">
+            <p>.ec-col-1 <div>{{i}}</div></p>
+          </div>
         </div>
       </div>
     </div>
     `,
   }))
-  .add('Full Width vs Normal', () => ({
+  .add('nested', () => ({
     template: `
     <div class="grid-story">
       <h1 class="grid-story__title">.ec-grid</h1>
-      <div class="ec-grid ec-grid--row-sm ec-grid--full-width grid-story__container">
-        <div
-          v-for="i in 12"
-          class="ec-col-1 grid-story__column">
-          <p>.ec-col-1</p>
-        </div>
-      </div>
 
-      <h1 class="grid-story__title">.ec-grid</h1>
-      <div class="ec-grid ec-grid--row-sm grid-story__container">
-        <div
-          v-for="i in 12"
-          class="ec-col-1 grid-story__column">
-          <p>.ec-col-1</p>
-        </div>
-      </div>
-    </div>
-    `,
-  }))
-  .add('Nested', () => ({
-    template: `
-    <div class="grid-story">
-      <h1 class="grid-story__title">.ec-grid</h1>
-      <div class="ec-grid ec-grid--row-sm grid-story__container">
-        <div class="ec-col-6 grid-story__column">
-          <h1 style="font-size: 20px;">.ec-col-6</h1>
-          <div style="border:1px solid black;padding:5px;margin:5px;">
+      <div class="ec-grid grid-story__container">
+        <div class="ec-grid__row">
+          <div class="ec-col-6 grid-story__column">
+            <h1 style="font-size: 20px;">.ec-col-6</h1>
+            <div style="border:1px solid black;padding:5px;margin:5px;">
+              <h1 style="font-size: 20px;">.ec-grid</h1>
+
+                <div class="ec-grid">
+                  <div class="ec-grid__row">
+                    <div
+                      v-for="i in 3"
+                      class="ec-col-4 grid-story__column">
+                      <p>.ec-col-4</p>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+
+          <div class="ec-col-6 grid-story__column">
+            <h1 style="font-size: 20px;">.ec-col-6</h1>
+            <div style="border:1px solid black;padding:5px;margin:5px;">
             <h1 style="font-size: 20px;">.ec-grid</h1>
-            <div class="ec-grid ec-grid--full-width">
-              <div
-                v-for="i in 3"
-                class="ec-col-4 grid-story__column">
-                <p>.ec-col-4</p>
+            <div class="ec-grid">
+              <div class="ec-grid__col">
+                <div
+                  v-for="i in 3"
+                  class="ec-col-4 grid-story__column">
+                  <p>.ec-col-4</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        <div class="ec-col-6 grid-story__column">
-          <p>.ec-col-6</p>
-        </div>
       </div>
     </div>
     `,
   }))
-
-
-  .add('Responsive', () => ({
+  .add('responsive', () => ({
     template: `
     <div class="grid-story">
-      <h1 class="grid-story__title">Responsive - Resize to see swap between column and row</h1>
-      <h1 class="grid-story__title">
-      ec-grid ec-grid--col-xs ec-grid--row-sm ec-grid--col--md ec-grid--row-lg ec-grid--col-xl ec-grid--row-2xl </h1>
+      <h1 class="grid-story__title">Resize to swap between column and row @xs @sm @md @lg @xl breakpoints</h1>
+      <h2 class="grid-story__title">Grandparent: ec-grid</h2>
+      <h2 class="grid-story__title"> Parent: ec-grid__col ec-grid__row-xs ec-grid__col-sm ec-grid__row-md ec-grid__col-lg ec-grid__row-xl</h2>
 
-      <div class="ec-grid ec-grid--col-xs ec-grid--row-sm ec-grid--col--md ec-grid--row-lg ec-grid--col-xl ec-grid--row-2xl grid-story__container">
-        <div
-          v-for="i in 4"
-          class="ec-col-3 grid-story__column">
-          <p>.ec-col-3</p>
+      <div class="ec-grid grid-story__container">
+        <div class="ec-grid__col ec-grid__row-xs ec-grid__col-sm ec-grid__row-md ec-grid__col-lg ec-grid__row-xl">
+          <div
+            v-for="i in 4"
+            class="ec-col-3 grid-story__column">
+            <p>.ec-col-3</p>
+          </div>
         </div>
       </div>
     </div>
   `,
   }))
-  .add('TFO example', () => ({
+  .add('tfo example', () => ({
     template: `
       <div class="ec-ml--24 ec-mr--24">
-        <div class="ec-grid ec-grid--full-width ec-align-center-md ec-grid--col-xs ec-grid--row-md">
-          <div class="ec-col-9">
-            <h1>Lending</h1>
-            <p> Here you will be able to keep track of all your requests.
+        <div class="ec-grid">
+          <div class="ec-grid__col ec-grid__row-md">
+            <div class="ec-col-9">
+              <h1>Trade Finance</h1>
+              <p> Here you will be able to keep track of all your requests.
+            </div>
+            <div class="ec-col-3" style="align-self: center;text-align:right;">
+              <button class="ec-column ec-btn ec-btn--md ec-btn--rounded ec-btn--primary">New Request</button>
+            </div>
           </div>
-
-          <button class="ec-col-3 ec-btn ec-btn--md ec-btn--rounded ec-btn--primary">New Request</button>
         </div>
 
-        <div class="ec-grid ec-grid--full-width ec-grid--col-xs ec-grid--row-md">
-          <div class="ec-col-8" style="height; border: 1px solid black;"> Credit line component</div>
-          <div class="ec-col-4" style="height:200px; border: 1px solid black;"> Management card component</div>
+        <div class="ec-grid">
+          <div class="ec-grid__col ec-grid__row-md">
+            <div class="ec-col-8" style="height; border: 1px solid black;"> Credit line component</div>
+            <div class="ec-col-4" style="height:200px; border: 1px solid black;"> Management card component</div>
+          </div>
         </div>
 
-        <div class="ec-grid ec-grid--full-width ec-grid--row-xs">
-          <div class="ec-col-12" style="border:1px solid black;height:200px;">
-            This is the requests container
-          </div>
+        <div class="ec-grid">
+          <div class="ec-grid__row">
+            <div class="ec-col-12" style="border:1px solid black;height:200px;">
+              This is the requests container
+            </div>
         </div>
       </div>
     `,
