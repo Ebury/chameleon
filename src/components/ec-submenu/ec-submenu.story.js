7ada22664b53d55c8ced7eb26a0438cffa87f46d
stavros <stavros.tomadakis@ebury.com>
2019-12-02 00:29:46 +0000

    basic functionality

--- src/components/ec-submenu/ec-submenu.story.js
+++ src/components/ec-submenu/ec-submenu.story.js
@@ -1,65 +1,73 @@
 import { storiesOf } from '@storybook/vue';
-import { array } from '@storybook/addon-knobs';
+import { array, select } from '@storybook/addon-knobs';
+import StoryRouter from 'storybook-vue-router';
 import EcSubmenu from './ec-submenu.vue';
 
 const stories = storiesOf('Submenu', module);
 
-const tabs = [
+const submenu = [
   {
-    headerSlotName: 'tab1',
-    mainSlotName: 'tab1Content',
+    headerSlotName: 'menu-item-1',
+    mainSlotName: 'menu-item-1-content',
     title: 'Submitted Requests',
     isActive: true,
   },
   {
-    headerSlotName: 'tab2',
-    mainSlotName: 'tab2Content',
+    headerSlotName: 'menu-item-2',
+    mainSlotName: 'menu-item-2-content',
     title: 'Repayments to Ebury',
     isActive: false,
   },
   {
-    headerSlotName: 'tab3',
-    mainSlotName: 'tab3Content',
+    headerSlotName: 'menu-item-3',
+    mainSlotName: 'menu-item-3-content',
     title: 'test',
     isActive: false,
   },
 ];
 
-stories.add('basic', () => ({
-  components: { EcSubmenu },
-  props: {
-    tabs: {
-      default: array('tabs', tabs),
+stories
+  .addDecorator(StoryRouter())
+  .add('basic', () => ({
+    components: { EcSubmenu },
+    props: {
+      submenu: {
+        default: array('submenu', submenu),
+      },
+      type: {
+        default: select('type', ['submenu', 'tabs'], 'submenu'),
+      },
     },
-  },
-  template: `
-    <ec-submenu :tabs="tabs">
-        <template #tab1>
-          Submitted Requests(30)
-        </template>
+    template: `
+    <div style="margin: 20px;">
+      <ec-submenu :submenu="submenu" :type="type">
+          <template #menu-item-1>
+            <a>Submitted Requests(30)</a>
+          </template>
 
-        <template #tab1Content>
-          <h1> One - 1 </h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, tempora esse? Fugit minus consequatur harum culpa sed laborum nulla expedita molestias, quia cupiditate commodi repudiandae labore dolore dolor. Eum sapiente repellendus tempora tenetur accusantium, molestias hic adipisci aspernatur, sed possimus explicabo culpa quas laudantium? Quidem sunt totam maxime magni minus dignissimos eos libero nobis accusantium aspernatur qui inventore odio adipisci at dolorum consectetur velit ipsa quia assumenda porro, consequatur nisi mollitia deserunt. Iusto possimus debitis assumenda harum atque libero voluptates fugit quibusdam ipsa voluptatibus eligendi ex doloremque ducimus molestias natus adipisci, earum facere repudiandae sint nobis error maiores iste. Quam.</p>
-        </template>
+          <template #menu-item-1-content>
+            <h1> One - 1 </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, tempora esse? Fugit minus consequatur harum culpa sed laborum nulla expedita molestias, quia cupiditate commodi repudiandae labore dolore dolor. Eum sapiente repellendus tempora tenetur accusantium, molestias hic adipisci aspernatur, sed possimus explicabo culpa quas laudantium? Quidem sunt totam maxime magni minus dignissimos eos libero nobis accusantium aspernatur qui inventore odio adipisci at dolorum consectetur velit ipsa quia assumenda porro, consequatur nisi mollitia deserunt. Iusto possimus debitis assumenda harum atque libero voluptates fugit quibusdam ipsa voluptatibus eligendi ex doloremque ducimus molestias natus adipisci, earum facere repudiandae sint nobis error maiores iste. Quam.</p>
+          </template>
 
-        <template #tab2>
-          Repayments to Ebury(28)
-        </template>
+          <template #menu-item-2>
+            <a>Repayments to Ebury(28)</a>
+          </template>
 
-        <template #tab2Content>
-          <h1> Two - 2 </h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, tempora esse? Fugit minus consequatur harum culpa sed laborum nulla expedita molestias, quia cupiditate commodi repudiandae labore dolore dolor. Eum sapiente repellendus tempora tenetur accusantium, molestias hic adipiscm.</p>
-        </template>
+          <template #menu-item-2-content>
+            <h1> Two - 2 </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, tempora esse? Fugit minus consequatur harum culpa sed laborum nulla expedita molestias, quia cupiditate commodi repudiandae labore dolore dolor. Eum sapiente repellendus tempora tenetur accusantium, molestias hic adipiscm.</p>
+          </template>
 
-        <template #tab3>
-          Just Testing(28)
-        </template>
+          <template #menu-item-3>
+            <a>Just Testing(28)</a>
+          </template>
 
-        <template #tab3Content>
-          <h1> Three - 3 </h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, tempora esse? Fugit minus consequatur harum culpa sed laborum nulla expedita molestias, quia cupiditate commodi repudiandae labore dolore dolor. Eum sapiente repellendus tempora tenetur accusantium, molestias hic adipisci aspernatur, sed possimus explicabo culpa quas laudantium? Quidem sunt totam maxime magni minus dignissimos eos libero nobis accusantium aspernatur qui inventore odio adipisci at dolorum consectetur velit ipsa quia assumenda porro, consequatur nisi mollitia deserunt. Iusto possimus debitis assumenda harum atque libero voluptates fugit quibusdam ipsa voluptatibus eligendi ex doloremque ducimus molestias natus adipisci, earum facere repudiandae sint nobis error maiores iste. Quam.</p>
-        </template>
-    </ec-submenu>
+          <template #menu-item-3-content>
+            <h1> Three - 3 </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, tempora esse? Fugit minus consequatur harum culpa sed laborum nulla expedita molestias, quia cupiditate commodi repudiandae labore dolore dolor. Eum sapiente repellendus tempora tenetur accusantium, molestias hic adipisci aspernatur, sed possimus explicabo culpa quas laudantium? Quidem sunt totam maxime magni minus dignissimos eos libero nobis accusantium aspernatur qui inventore odio adipisci at dolorum consectetur velit ipsa quia assumenda porro, consequatur nisi mollitia deserunt. Iusto possimus debitis assumenda harum atque libero voluptates fugit quibusdam ipsa voluptatibus eligendi ex doloremque ducimus molestias natus adipisci, earum facere repudiandae sint nobis error maiores iste. Quam.</p>
+          </template>
+      </ec-submenu>
+    </div>
     `,
-}));
+  }));
--- src/components/ec-submenu/ec-submenu.vue
+++ src/components/ec-submenu/ec-submenu.vue
@@ -1,89 +1,211 @@
 <template>
-  <div class="ec-tabs">
-    <ul class="ec-tabs__header">
-      <li
-        v-for="(tab, index) in tabs"
-        :key="tab.index"
-        class="ec-tabs__header-item"
-
-        :class="{'ec-tabs__header-item-active': tab.isActive}"
-        @click="makeActive(index)"
+  <div
+    ref="submenu"
+    class="ec-submenu"
+    :class="{'ec-submenu--tabs': type === 'tabs'}"
+  >
+    <ec-icon
+      v-if="headerOverflows"
+      class="ec-submenu__arrow-left"
+      name="simple-chevron-left"
+      :size="24"
+      @click="moveLeft"
+    />
+    <div
+      ref="header"
+      class="ec-submenu__header-container"
+    >
+      <ul
+        class="ec-submenu__header"
+        size="24"
       >
-        <slot :name="tab.headerSlotName" />
-      </li>
-    </ul>
-
-    <main class="ec-tabs__main">
-      <transition name="ec-tabs__fade">
-        <div
-          v-for="tab in tabs"
-          :key="tab.id"
-          v-show="tab.isActive"
-          :class="{isActive: tab.isActive}"
+        <li
+          v-for="(menuItem, index) in submenu"
+          :key="menuItem.index"
+          class="ec-submenu__header-item"
+          :class="{'ec-submenu__header-item-active': menuItem.isActive}"
+          @click="makeActive(index)"
         >
-          <slot
-            :name="tab.mainSlotName"
-          />
-        </div>
-      </transition></main>
+          <slot :name="menuItem.headerSlotName" />
+        </li>
+      </ul>
+    </div>
+
+    <ec-icon
+      v-if="headerOverflows"
+      class="ec-submenu__arrow-right"
+      name="simple-chevron-right"
+      :size="24"
+      @click="moveRight"
+    />
+
+    <main class="ec-submenu__main">
+      <div
+        v-for="menuItem in submenu"
+        :key="menuItem.id"
+        v-show="menuItem.isActive"
+        :class="{isActive: menuItem.isActive}"
+      >
+        <slot
+          :name="menuItem.mainSlotName"
+        />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
+import EcIcon from '@/components/ec-icon';
+
 export default {
   name: 'EcSubmenu',
+  components: { EcIcon },
   props: {
-    tabs: {
+    submenu: {
       type: Array,
       required: true,
     },
+    type: {
+      type: String,
+      default: 'submenu',
+    },
+  },
+  data() {
+    return {
+      windowWidth: 0,
+      headerWidth: 0,
+      headerOverflows: false,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.getWindowWidth);
+      window.addEventListener('load', this.getWindowWidth);
+      window.addEventListener('resize', this.headerIsOverflowing);
+      window.addEventListener('load', this.headerIsOverflowing);
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.getWindowWidth);
+    window.removeEventListener('load', this.getWindowWidth);
+    window.removeEventListener('resize', this.headerIsOverflowing);
+    window.removeEventListener('load', this.headerIsOverflowing);
   },
   methods: {
     makeActive(index) {
-      for (const tab of this.tabs) {
-        tab.isActive = false;
+      for (const menuItem of this.submenu) {
+        menuItem.isActive = false;
       }
-      this.tabs[index].isActive = true;
+      this.submenu[index].isActive = true;
+    },
+    getWindowWidth() {
+      this.windowWidth = this.$refs.submenu.clientWidth;
+      console.log(`to window width einai${this.windowWidth}`);
+    },
+    headerIsOverflowing() {
+      this.headerWidth = this.$refs.header.scrollWidth;
+      console.log(`to header width einai${this.headerWidth}`);
+      if (this.headerWidth > this.windowWidth) {
+        this.headerOverflows = true;
+      } else {
+        this.headerOverflows = false;
+      }
+    },
+    moveRight() {
+      this.$refs.header.scrollBy(100, 0);
+    },
+    moveLeft() {
+      this.$refs.header.scrollBy(-100, 0);
     },
   },
 };
 </script>
 
 <style lang="scss">
-@import '../../scss/settings/colors/index';
+@import '../../scss/settings/index';
 @import '../../scss/tools/index';
 
-.ec-tabs {
-  padding: 5px;
-  margin: 5px;
+$ec-submenu-body: $level-3-body-and-headings !default;
+$ec-submenu-hover: $level-4-tech-blue !default;
+$ec-submenu-disabled: $level-6-disabled-lines !default;
+$ec-submenu-white: $white !default;
+
+.ec-submenu {
+  position: relative;
+  z-index: 0;
+
+  &__arrow-left,
+  &__arrow-right {
+    background: $white;
+    top: 8px;
+    position: absolute;
+    z-index: 1;
+  }
+
+  &__arrow-left {
+    left: 0;
+  }
+
+  &__arrow-right {
+    right: 0;
+  }
 
   &__header {
-    overflow: auto;
-    display: flex;
     min-width: 100px;
-    border-bottom: 1px solid black;
+    border-bottom: 1px solid $ec-submenu-disabled;
+    margin-bottom: 12px;
+    white-space: nowrap;
+
+    &-container {
+      overflow-x: auto;
+    }
   }
 
   &__header-item {
-    padding: 16px 16px 0 16px;
-    text-align: center;
+    bottom: -1px;
     cursor: pointer;
+    display: inline-block;
+    padding: 8px 16px 4px 16px;
+    position: relative;
+    text-align: center;
+
+    .ec-submenu--tabs & {
+      padding-bottom: 8px;
+    }
+
+    a {
+      text-decoration: none;
+      color: $ec-submenu-body;
+      transition: color 0.3s ease-out;
+
+      &:hover {
+        color: $ec-submenu-hover;
+      }
+    }
   }
 
   &__header-item-active {
-    border: 1px solid black;
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
+    border-bottom-width: 1px;
+    border-style: solid;
+    border-color: $ec-submenu-hover;
+    cursor: auto;
+
+    a {
+      color: $ec-submenu-hover;
+    }
+
+    .ec-submenu--tabs & {
+      border-top-width: 1px;
+      border-left-width: 1px;
+      border-right-width: 1px;
+      border-color: $ec-submenu-disabled $ec-submenu-disabled  $ec-submenu-white $ec-submenu-disabled;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+    }
   }
 
-  // &__content {
+  // &__fade {
+  //   @include fade-transition;
   // }
-
-  // &__content-panel {
-  // }
-
-  &__fade {
-    @include fade-transition;
-  }
 }
 </style>
