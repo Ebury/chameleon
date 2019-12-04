4fe3b5ee152efd92b134381ce64c65c68ce71e21
stavros <stavros.tomadakis@ebury.com>
2019-12-04 09:53:54 +0000

    various small amends

--- src/scss/components/ec-grid/ec-grid.story.js
+++ src/scss/components/ec-grid/ec-grid.story.js
@@ -6,71 +6,71 @@ const stories = storiesOf('Grid', module);
 stories
   .add('rows', () => ({
     template: `
-    <div class="grid-story">
-      <h1 class="grid-story__title">12 Columns - 24 pixel gutters</h1>
-      <h2 class="grid-story__title">Grandparent: "ec-grid", Parent: "ec-grid__row" Child: "ec-col-1 to ec-12"</h2>
-      <h2 class="grid-story__title">Normal</h2>
+    <div class="ec-grid-story">
+      <h1 class="ec-grid-story__title">12 Columns - 24 pixel gutters</h1>
+      <h2 class="ec-grid-story__title">Grandparent: "ec-grid", Parent: "ec-grid__row" Child: "ec-col-1 to ec-12"</h2>
+      <h2 class="ec-grid-story__title">Normal</h2>
       <div class="ec-grid">
-        <div class="ec-grid__row grid-story__container">
+        <div class="ec-grid__row ec-grid-story__container">
           <div
             v-for="i in 12"
-            class="ec-col-1 grid-story__column">
+            class="ec-col-1 ec-grid-story__column">
             <p>.ec-col-1 <div>{{ i }}</div></p>
           </div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">Reverse</h1>
+      <h1 class="ec-grid-story__title">Reverse</h1>
       <div class="ec-grid">
-        <div class="ec-grid__row--reverse grid-story__container">
+        <div class="ec-grid__row--reverse ec-grid-story__container">
           <div
             v-for="i in 12"
-            class="ec-col-1 grid-story__column">
+            class="ec-col-1 ec-grid-story__column">
             <p>.ec-col-1 <div>{{ i }}</div></p>
           </div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">Wrap</h1>
+      <h1 class="ec-grid-story__title">Wrap</h1>
       <div class="ec-grid">
-        <div class="ec-grid__row ec-grid__row--wrap grid-story__container">
+        <div class="ec-grid__row ec-grid__row--wrap ec-grid-story__container">
           <div
             v-for="i in 18"
-            class="ec-col-1 grid-story__column">
+            class="ec-col-1 ec-grid-story__column">
             <p>.ec-col-1 <div>{{ i }}</div></p>
           </div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">Offset</h1>
+      <h1 class="ec-grid-story__title">Offset</h1>
       <div class="ec-grid">
-        <div class="ec-grid__row grid-story__container">
-          <div class="ec-col-1 ec-offset-1 grid-story__column"><p>.ec-offset-1</p></div>
-          <div class="ec-col-1 ec-offset-2 grid-story__column"><p>.ec-offset-2</p></div>
-          <div class="ec-col-1 ec-offset-4 grid-story__column"><p>.ec-offset-4</p></div>
+        <div class="ec-grid__row ec-grid-story__container">
+          <div class="ec-col-1 ec-offset-1 ec-grid-story__column"><p>.ec-offset-1</p></div>
+          <div class="ec-col-1 ec-offset-2 ec-grid-story__column"><p>.ec-offset-2</p></div>
+          <div class="ec-col-1 ec-offset-4 ec-grid-story__column"><p>.ec-offset-4</p></div>
         </div>
       </div>
 
-      <h1 class="grid-story__title">Various column combinations</h1>
+      <h1 class="ec-grid-story__title">Various column combinations</h1>
       <div class="ec-grid">
-        <div class="ec-grid__row grid-story__container">
-          <div class="ec-col-2 grid-story__column"><p>.ec-col-2</p></div>
-          <div class="ec-col-4 grid-story__column"><p>.ec-col-4</p></div>
-          <div class="ec-col-6 grid-story__column"><p>.ec-col-6</p></div>
+        <div class="ec-grid__row ec-grid-story__container">
+          <div class="ec-col-2 ec-grid-story__column"><p>.ec-col-2</p></div>
+          <div class="ec-col-4 ec-grid-story__column"><p>.ec-col-4</p></div>
+          <div class="ec-col-6 ec-grid-story__column"><p>.ec-col-6</p></div>
         </div>
       </div>
     `,
   }))
   .add('columns', () => ({
     template: `
-    <div class="grid-story">
-      <h1 class="grid-story__title">12 Columns - 24 pixel gutters</h1>
-      <h2 class="grid-story__title">Grandparent: "ec-grid", Parent: "ec-grid__col" Child: "ec-col-1 to ec-12"</h2>
+    <div class="ec-grid-story">
+      <h1 class="ec-grid-story__title">12 Columns - 24 pixel gutters</h1>
+      <h2 class="ec-grid-story__title">Grandparent: "ec-grid", Parent: "ec-grid__col" Child: "ec-col-1 to ec-12"</h2>
       <div class="ec-grid">
-        <div class="ec-grid__col grid-story__container">
+        <div class="ec-grid__col ec-grid-story__container">
           <div
             v-for="i in 12"
-            class="ec-col-1 grid-story__column">
+            class="ec-col-1 ec-grid-story__column">
             <p>.ec-col-1 <div>{{i}}</div></p>
           </div>
         </div>
@@ -80,12 +80,12 @@ stories
   }))
   .add('nested', () => ({
     template: `
-    <div class="grid-story">
-      <h1 class="grid-story__title">.ec-grid</h1>
+    <div class="ec-grid-story">
+      <h1 class="ec-grid-story__title">.ec-grid</h1>
 
-      <div class="ec-grid grid-story__container">
+      <div class="ec-grid ec-grid-story__container">
         <div class="ec-grid__row">
-          <div class="ec-col-6 grid-story__column">
+          <div class="ec-col-6 ec-grid-story__column">
             <h1 style="font-size: 20px;">.ec-col-6</h1>
             <div style="border:1px solid black;padding:5px;margin:5px;">
               <h1 style="font-size: 20px;">.ec-grid</h1>
@@ -94,7 +94,7 @@ stories
                   <div class="ec-grid__row">
                     <div
                       v-for="i in 3"
-                      class="ec-col-4 grid-story__column">
+                      class="ec-col-4 ec-grid-story__column">
                       <p>.ec-col-4</p>
                     </div>
                   </div>
@@ -102,7 +102,7 @@ stories
             </div>
           </div>
 
-          <div class="ec-col-6 grid-story__column">
+          <div class="ec-col-6 ec-grid-story__column">
             <h1 style="font-size: 20px;">.ec-col-6</h1>
             <div style="border:1px solid black;padding:5px;margin:5px;">
             <h1 style="font-size: 20px;">.ec-grid</h1>
@@ -110,7 +110,7 @@ stories
               <div class="ec-grid__col">
                 <div
                   v-for="i in 3"
-                  class="ec-col-4 grid-story__column">
+                  class="ec-col-4 ec-grid-story__column">
                   <p>.ec-col-4</p>
                 </div>
               </div>
@@ -123,16 +123,16 @@ stories
   }))
   .add('responsive', () => ({
     template: `
-    <div class="grid-story">
-      <h1 class="grid-story__title">Resize to swap between column and row @xs @sm @md @lg @xl breakpoints</h1>
-      <h2 class="grid-story__title">Grandparent: ec-grid</h2>
-      <h2 class="grid-story__title"> Parent: ec-grid__col ec-grid__row@xs ec-grid__col@sm ec-grid__row@md ec-grid__col@lg ec-grid__row@xl</h2>
+    <div class="ec-grid-story">
+      <h1 class="ec-grid-story__title">Resize to swap between column and row @xs @sm @md @lg @xl breakpoints</h1>
+      <h2 class="ec-grid-story__title">Grandparent: ec-grid</h2>
+      <h2 class="ec-grid-story__title"> Parent: ec-grid__col ec-grid__row@xs ec-grid__col@sm ec-grid__row@md ec-grid__col@lg ec-grid__row@xl</h2>
 
-      <div class="ec-grid grid-story__container">
+      <div class="ec-grid ec-grid-story__container">
         <div class="ec-grid__col ec-grid__row@xs ec-grid__col@sm ec-grid__row@md ec-grid__col@lg ec-grid__row@xl">
           <div
             v-for="i in 4"
-            class="ec-col-3 grid-story__column">
+            class="ec-col-3 ec-grid-story__column">
             <p>.ec-col-3</p>
           </div>
         </div>
