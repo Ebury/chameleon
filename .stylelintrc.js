c8d5a41913d91d851c5eac91e4eddccfaccd9671
mcibique <mcibique@users.noreply.github.com>
2020-04-27 19:33:57 +0200

    ONL-4414: feat: Added tailwindcss. (#136)
    * ONL-4414: feat: Added tailwindcss.
    
    * ONL-4414: wip
    
    * ONL-4414: fix: Moved tailwind little up in the postcss chain, so it can work with nested media queries and mixin.
    
    * 0.1.83

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -39,7 +39,7 @@ module.exports = {
     'scss/at-each-key-value-single-line': true,
     'scss/at-rule-conditional-no-parentheses': true,
     'scss/at-rule-no-unknown': true,
-    'scss/comment-no-loud': true,
+    'scss/comment-no-loud': null, // disabled because of PostCSS migration
     'scss/declaration-nested-properties': null,
     'scss/declaration-nested-properties-no-divided-groups': true,
     'scss/dimension-no-non-numeric-values': true,
--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,14 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-mixins': {},
-    'postcss-nested': {},
-    'postcss-preset-env': {},
-    'postcss-color-function': {},
-    autoprefixer: {},
-  },
+module.exports = ({ file }) => {
+  // TODO: add purgecss and purge every file except tailwind.story.css
+  // eslint-disable-next-line no-unused-vars
+  const purgeFile = file.basename !== 'tailwind.story.css';
+  return {
+    plugins: {
+      'postcss-import': {},
+      'postcss-mixins': {},
+      tailwindcss: {},
+      'postcss-nested': {},
+      'postcss-preset-env': {},
+    },
+  };
 };
