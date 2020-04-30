4eca9f92fd19042b8e681657cabffb44deea5445
mcibique <mcibique@users.noreply.github.com>
2020-04-30 17:26:38 +0200

    ONL-4415: feat: Added purgecss. (#138)
    * ONL-4415: feat: Added purgecss.
    
    * ONL-4415: feat: Tailwind 1.4.0.
    
    * Update README.md
    
    Co-authored-by: Carlos Matallín Civera <36676+matallo@users.noreply.github.com>
    
    * 0.1.86
    
    Co-authored-by: Carlos Matallín Civera <36676+matallo@users.noreply.github.com>

--- src/styles/utilities/typography.story.js
+++ src/styles/utilities/typography.story.js
@@ -6,11 +6,11 @@ stories.add('basic', () => ({
   data() {
     return {
       types: [
-        'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-        'body-text', 'body-strong',
-        'mini-header', 'table-header',
-        'small-text', 'caption-text',
-        'input-label', 'btn-text', 'flags-text',
+        'tw-h1', 'tw-h2', 'tw-h3', 'tw-h4', 'tw-h5', 'tw-h6',
+        'tw-body-text', 'tw-body-strong',
+        'tw-mini-header', 'tw-table-header',
+        'tw-small-text', 'tw-caption-text',
+        'tw-input-label', 'tw-btn-text', 'tw-flags-text',
       ],
     };
   },
@@ -18,7 +18,7 @@ stories.add('basic', () => ({
     <div>
       <p v-for="type of types"
         :key="type"
-        :class="'tw-px-24 tw-' + type">{{ type }} - Lorem ipsum dolor sit amet, consectetur adipiscing <a href="#">elit</a>. Donec sodales felis nec libero vehicula, sit amet gravida dolor cursus</p>
+        :class="'tw-px-24 ' + type">{{ type.replace('tw-', '') }} - Lorem ipsum dolor sit amet, consectetur adipiscing <a href="#">elit</a>. Donec sodales felis nec libero vehicula, sit amet gravida dolor cursus</p>
     </div>
   `,
 }));
