430144489c3314b7465d2733fdac597de820b3f8
Fco. Javier Mesa Olmo <francisco.mesa@ebury.com>
2020-05-15 20:03:02 +0200

    ONL-4524:feat: PR comments

--- src/styles/components/ec-card/ec-card.story.js
+++ src/styles/components/ec-card/ec-card.story.js
@@ -15,6 +15,11 @@ stories
             class: '',
           },
           {
+            title: 'Basic with a long text',
+            text: 'Methionylthreonylthreonyglutaminylarginyl|Lopadotemachoselachogaleokranioleipsan|Pneumonoultramicroscopicsilicovolcanoconiosis',
+            class: '',
+          },
+          {
             title: 'Interactive card',
             text: 'Interactive example card',
             class: 'ec-card--is-interactive',
@@ -26,37 +31,10 @@ stories
     <div class="tw-p-16">
       <div v-for="(card, index) in list" :key="index">
         <h3>{{ card.title }}</h3>
-        <div class="tw-flex tw-m-12">
-          <div style="width: 33vw;">
+        <div class="tw-m-12">
+          <div class="tw-w-1/3">
             <div class="ec-card" :class="card.class">
               {{ card.text }}
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Trade finance cards -->
-      <h3>Trade finance cards</h3>
-      <div class="tw-flex tw-m-12">
-        <div style="width: 66vw;">
-          <div class="ec-card">
-            <div class="tw-p-8">
-              <div class="tw-mb-24" style="font-size:25px; line-height: 35px;">
-                Credit line: EUR 1,000,000.00
-              </div>
-              <ec-icon class="tw-mr-8" name="simple-check" :size="108" />
-              <ec-icon class="tw-mr-8" name="simple-check" :size="14" />
-              Used EUR 0.00
-            </div>
-          </div>
-        </div>
-        <div class="tw-m-12" style="width: 33vw;">
-          <div class="ec-card" >
-            <div class="tw-text-center" style="font-size:18px; line-height: 28px;">
-              Management account status
-            </div>
-            <ec-icon class="tw-my-24" name="simple-check" :size="48" />
-            <div class="tw-text-center tw-small-text">
-              Here we will display the status of your management accounts, so you can update them when necessary.
             </div>
           </div>
         </div>
