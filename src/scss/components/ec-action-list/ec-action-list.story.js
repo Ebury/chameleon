6faed7da70928f1f285f022b9fd89460b9aa2edc
stavros <stavros.tomadakis@ebury.com>
2020-05-21 23:06:46 +0100

    ONL-4524:feat: refactor component to use one story and update snapshots

--- src/scss/components/ec-action-list/ec-action-list.story.js
+++ src/scss/components/ec-action-list/ec-action-list.story.js
@@ -3,46 +3,49 @@ import { storiesOf } from '@storybook/vue';
 const stories = storiesOf('Action List', module);
 
 stories
-  .add('vertical', () => ({
+  .add('all', () => ({
     template: `
-    <div style="display: flex; height: 100vh; margin: 10px">
-      <div style="margin: auto; width: 33vw;">
-        <div class="ec-action-list-group ec-action-list-group--column">
+    <div>
+      <div class="ec-m--20">
+        <h3 style="margin-left:4px;">Horizontal</h3>
+
+        <div class="ec-action-list-group">
+          <div class="ec-action-list-item">
+            <div>Action title</div>
+            <div style="color: #86989c;">Lorem ipsum dolor consectutut</div>
+          </div>
+
+          <div class="ec-action-list-item">
+            <div>Action title</div>
+            <div style="color: #86989c;">Lorem ipsum dolor consectutut</div>
+          </div>
+
+          <div class="ec-action-list-item">
+            <div>Action title</div>
+            <div style="color: #86989c;">Lorem ipsum dolor consectutut</div>
+          </div>
+        </div>
+
+        <h3 style="margin: 20px 0 4px">Vertical</h3>
+
+        <div class="ec-action-list-group ec-action-list-group--column" style="max-width:33vw;" >
           <div class="ec-action-list-item">
             <div>Action title</div>
             <div style="color: #86989c;">Lorem ipsum dolor consectutur</div>
           </div>
+
           <div class="ec-action-list-item">
             <div>Action title</div>
             <div style="color: #86989c;">Lorem ipsum dolor consectutur</div>
           </div>
+
           <div class="ec-action-list-item">
             <div>Action title</div>
             <div style="color: #86989c;">Lorem ipsum dolor consectutur</div>
           </div>
         </div>
       </div>
-    </div>
-      `,
-  }))
-  .add('horizontal', () => ({
-    template: `
-    <div style="display: flex; height: 100vh; margin: 10px">
-      <div style="margin: auto; width: 66vw;">
-        <div class="ec-action-list-group">
-          <div class="ec-action-list-item">
-            <div>Action title</div>
-            <div style="color: #86989c;">Lorem ipsum dolor consectutut</div>
-          </div>
-          <div class="ec-action-list-item">
-            <div>Action title</div>
-            <div style="color: #86989c;">Lorem ipsum dolor consectutut</div>
-          </div>
-          <div class="ec-action-list-item">
-            <div>Action title</div>
-            <div style="color: #86989c;">Lorem ipsum dolor consectutut</div>
-          </div>
-        </div>
+
       </div>
     </div>
       `,
