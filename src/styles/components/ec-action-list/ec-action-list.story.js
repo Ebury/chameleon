ed1704ac1c92cf973922846c4026691909ad73c1
stavros-tomas <55188371+stavros-tomas@users.noreply.github.com>
2020-05-22 17:47:25 +0100

    ONL-4423 Migrate actions list  (#192)
    * ONL-4524:feat: refactor component to use one story and update snapshots
    
    * ONL-4423:refactor: move files from sass directory to styles and apply tailwind classes
    
    * ONL-4423:update integrations tests for actions list
    
    * 0.1.97

---
+++ src/styles/components/ec-action-list/ec-action-list.story.js
@@ -0,0 +1,48 @@
+import { storiesOf } from '@storybook/vue';
+
+const stories = storiesOf('Action List', module);
+
+stories
+  .add('all', () => ({
+    template: `
+    <div class="tw-m-20">
+      <h3 class="tw-ml-4">Horizontal</h3>
+
+      <div class="ec-action-list-group">
+        <div class="ec-action-list-item">
+          <div>Action title</div>
+          <div class="tw-text-gray-5">Lorem ipsum dolor consectutut</div>
+        </div>
+
+        <div class="ec-action-list-item">
+          <div>Action title</div>
+          <div class="tw-text-gray-5">Lorem ipsum dolor consectutut</div>
+        </div>
+
+        <div class="ec-action-list-item">
+          <div>Action title</div>
+          <div class="tw-text-gray-5">Lorem ipsum dolor consectutut</div>
+        </div>
+      </div>
+
+      <h3 class="tw-mt-20 tw-ml-4">Vertical</h3>
+
+      <div class="ec-action-list-group ec-action-list-group--column tw-w-1/3">
+        <div class="ec-action-list-item">
+          <div>Action title</div>
+          <div class="tw-text-gray-5">Lorem ipsum dolor consectutur</div>
+        </div>
+
+        <div class="ec-action-list-item">
+          <div>Action title</div>
+          <div class="tw-text-gray-5">Lorem ipsum dolor consectutur</div>
+        </div>
+
+        <div class="ec-action-list-item">
+          <div>Action title</div>
+          <div class="tw-text-gray-5">Lorem ipsum dolor consectutur</div>
+        </div>
+      </div>
+    </div>
+      `,
+  }));
