d218778bbc1de8073fb36e8892204b8c1b4c0518
jaimeperezpedra <jaimeperezpedra@gmail.com>
2020-06-15 15:44:13 +0100

    ONL-4614 fix story html and css classes

--- src/components/ec-toaster/ec-toaster.story.js
+++ src/components/ec-toaster/ec-toaster.story.js
@@ -73,13 +73,16 @@ stories.add('basic', () => ({
   // eslint-disable-next-line no-unused-vars
   render(h) {
     return (
-      <div class="tw-fixed tw-right-0 tw-top-0 tw-w-full" style={{
-        maxWidth: '400px',
-      }}>
-        <EcToaster messages={this.messages} onRemove={message => this.removeMessage(message)} />
+      <div class="tw-flex tw-items-center tw-justify-center tw-h-screen">
+        <div class="tw-fixed tw-right-0 tw-top-0 tw-w-full" style={{
+          maxWidth: '400px',
+        }}>
+          <EcToaster messages={this.messages} onRemove={message => this.removeMessage(message)} />
+        </div>
         <button
-          class="ec-btn ec-btn--md ec-btn--rounded ec-btn--primary tw-fixed tw-left-1/2 tw-top-1/2 tw--translate-x-1/2 tw--translate-y-1/2"
+          class="ec-btn ec-btn--md ec-btn--rounded ec-btn--primary"
           onClick={this.addMessage}>Add another message</button>
+
       </div>
     );
   },
