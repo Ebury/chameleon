bc339f92e6039517cca5cefe24e12a3e49dc59af
stavros-tomas <55188371+stavros-tomas@users.noreply.github.com>
2020-06-18 10:15:45 +0100

    Onl 4547- move to a viewport fixed panel (#244)

--- src/components/ec-panel/ec-panel.story.js
+++ src/components/ec-panel/ec-panel.story.js
@@ -1,6 +1,6 @@
 
 import { action } from '@storybook/addon-actions';
-import { boolean } from '@storybook/addon-knobs';
+import { boolean, number } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/vue';
 import EcPanel from './ec-panel.vue';
 
@@ -11,8 +11,17 @@ storiesOf('Panel', module)
       showFromProps: {
         default: boolean('Show Panel', true),
       },
-      withOverflowingContent: {
-        default: boolean('With overflowing content', true),
+      isHeaderEnabled: {
+        default: boolean('Show Header', true),
+      },
+      isFooterEnabled: {
+        default: boolean('Show Footer', true),
+      },
+      numberOfBodyParagraphs: {
+        default: number('Body paragraphs', 3),
+      },
+      numberOfPanelParagraphs: {
+        default: number('Panel paragraphs', 1),
       },
     },
     watch: {
@@ -35,26 +44,36 @@ storiesOf('Panel', module)
       },
     },
     template: `
-      <div style="width: 100vw; height: 100vh; position: fixed; top: 0; left: 0;">
-        <div v-for="i in 5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nobis obcaecati optio magnam, porro inventore? Suscipit sit atque a! Ullam provident quidem recusandae, itaque error labore porro inventore? Suscipit sit atque a! Ullam provident quidem recusandae, itaque error labore</div>
+      <div class="tw-bg-gray-7 tw-min-h-screen">
+        <div class="tw-max-w-screen-lg tw-my-0 tw-mx-auto tw-bg-light ec-panel-container tw-p-24">
+          <h1 class="tw-m-24">Panel story</h1>
 
-        <ec-panel v-model="show" @back="clickBackButton()">
-          <template #header>
-            <h2 class="ec-mb--24">Lorem, ipsum dolor sit amet</h2>
-          </template>
+          <div v-for="i in numberOfBodyParagraphs" class="tw-m-24">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nobis obcaecati optio magnam, porro inventore? Suscipit sit atque a! Ullam provident quidem recusandae, itaque error labore porro inventore? Suscipit sit atque a! Ullam provident quidem recusandae, itaque error labore</div>
 
-          <template #main>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nobis obcaecati optio magnam, porro inventore? Suscipit sit atque a! Ullam provident quidem recusandae</p>
+          <ec-panel v-model="show" @back="clickBackButton()">
+            <template v-if="isHeaderEnabled" #header>
+              <h3 class="tw-mb-24">Header</h3>
 
-            <div v-if="withOverflowingContent">
-              <p v-for="i in 10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nobis obcaecati optio magnam, porro inventore? Suscipit sit atque a! Ullam provident quidem recusandae</p>
-            </div>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </template>
 
-            <div class="ec-mt--36" style="text-align: center;">
-              <a href="#" class="ec-btn ec-btn--primary ec-btn--rounded ec-btn--md">Lorem, ipsum</a>
-            </div>
-          </template>
-        </ec-panel>
+            <template #main>
+              <h3>Main</h3>
+
+              <p v-for="i in numberOfPanelParagraphs">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nobis obcaecati optio magnam, porro inventore?</p>
+            </template>
+
+            <template v-if="isFooterEnabled" #footer>
+              <h3>Footer</h3>
+
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+              <div class="tw-mt-36 tw-text-center">
+                <a href="#" class="ec-btn ec-btn--primary ec-btn--rounded ec-btn--md">Lorem, ipsum</a>
+              </div>
+            </template>
+          </ec-panel>
+        </div>
       </div>
     `,
   }));
