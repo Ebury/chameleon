4839cd506d8f63dd243d7c2fb58d6e79f9574642
Milos Cibulka <mcibique@users.noreply.github.com>
2020-06-29 14:59:37 +0100

    ONL-4670: feat: Migrated amount input component.

--- src/components/ec-amount-input/ec-amount-input.story.js
+++ src/components/ec-amount-input/ec-amount-input.story.js
@@ -13,6 +13,14 @@ stories
         value: null,
       };
     },
+    watch: {
+      valueFromProps: {
+        immediate: true,
+        handler(newValue) {
+          this.value = newValue;
+        },
+      },
+    },
     props: {
       isMasked: {
         default: boolean('Is Masked', false),
@@ -26,6 +34,9 @@ stories
       label: {
         default: text('Label', 'Amount input'),
       },
+      valueFromProps: {
+        default: text('Value', ''),
+      },
     },
     methods: {
       getValueType() {
@@ -35,17 +46,23 @@ stories
       onInput: action('input'),
     },
     template: `
-    <div class="ec-ml--24 ec-mr--24">
-      <div class="ec-grid">
-        <div class="ec-grid__row">
-          <div class="ec-col-12">
-            <div class="ec-m--24">
+      <div class="tw-grid-container">
+        <div class="tw-grid">
+          <div class="tw-col-12">
+            <div>
               <ec-amount-input v-bind="$props" v-model="value" @change="onChange" @input="onInput" />
             </div>
-            <div class="ec-m--24">The input value: {{ value }} (type: {{ getValueType() }})</div>
+            <div class="tw-mt-24">The input value: {{ value }} (type: {{ getValueType() }})</div>
           </div>
         </div>
       </div>
-    </div>
-  `,
-  }));
+    `,
+  }), {
+    visualRegressionTests: {
+      knobs: {
+        'with-value': { Value: '1234567.89' },
+        'with-masked-value': { Value: '1234567.89', 'Is Masked': true },
+        'locale-with-no-decimals': { Value: '1234567.89', Locale: 'de-ch', Currency: 'JPY' },
+      },
+    },
+  });
