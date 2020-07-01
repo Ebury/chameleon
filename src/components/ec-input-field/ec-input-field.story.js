86f5d03dfec91d67d9acdfd175f6749010f251ab
mcibique <mcibique@users.noreply.github.com>
2020-07-01 10:08:22 +0200

    ONL-4671: feat: Migrated input-field component. (#277)

--- src/components/ec-input-field/ec-input-field.story.js
+++ src/components/ec-input-field/ec-input-field.story.js
@@ -26,7 +26,7 @@ stories
         default: text('note', 'Max 80 chars', GROUPS.NUMBER),
       },
       errorMessageNumber: {
-        default: text('errorMessage', '', GROUPS.NUMBER),
+        default: text('error message', '', GROUPS.NUMBER),
       },
       iconNumber: {
         default: text('icon', '', GROUPS.NUMBER),
@@ -41,7 +41,7 @@ stories
         default: text('note', 'Max 80 chars', GROUPS.TEXT),
       },
       errorMessageText: {
-        default: text('errorMessage', '', GROUPS.TEXT),
+        default: text('error message', '', GROUPS.TEXT),
       },
       iconText: {
         default: text('icon', '', GROUPS.TEXT),
@@ -53,13 +53,13 @@ stories
         default: text('label', 'Date input', GROUPS.DATE),
       },
       labelTooltip: {
-        default: text('tooltip lable text', 'Tooltip text', GROUPS.TOOLTIP),
+        default: text('tooltip label text', 'Tooltip text', GROUPS.TOOLTIP),
       },
       noteDate: {
         default: text('note', 'Max 80 chars', GROUPS.DATE),
       },
       errorMessageDate: {
-        default: text('errorMessage', '', GROUPS.DATE),
+        default: text('error message', '', GROUPS.DATE),
       },
       iconDate: {
         default: text('icon', '', GROUPS.DATE),
@@ -100,65 +100,55 @@ stories
       onInput: action('input'),
     },
     template: `
-    <div class="ec-ml--24 ec-mr--24">
-      <div class="ec-grid">
-        <div class="ec-grid__row">
-          <div class="ec-col-3">
-            <div class="ec-m--24">
-              <ec-input-field type="number" min="5" max="10" v-model.number="valueNumber" :note="noteNumber" :label="labelNumber" :error-message="errorMessageNumber" :icon="iconNumber" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
-            </div>
-          </div>
-
-          <div class="ec-col-4">
-            <div class="ec-m--24">
-              <ec-input-field type="text" placeholder="My input" v-model="valueText" :note="noteText" :label="labelText" :error-message="errorMessageText" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
-            </div>
-          </div>
-
-          <div class="ec-col-5">
-            <div class="ec-m--24">
-              <ec-input-field type="date" placeholder="My input" v-model="valueDate" :note="noteDate" :label="labelDate" :error-message="errorMessageDate" :icon="iconDate" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
-            </div>
-          </div>
-
-          <div class="ec-col-6">
-            <div class="ec-m--24">
-              <ec-input-field disabled placeholder="My input disabled" v-model="valueText" label="Input disabled" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
-            </div>
-          </div>
-
-          <div class="ec-col-6">
-            <div class="ec-m--24">
-              <ec-input-field disabled placeholder="My input disabled" v-model="valueText" label="Input disabled" error-message="Disabled with error" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
-            </div>
-          </div>
-
-          <div class="ec-col-3 ec-p--24">
-            <ec-input-field placeholder="My input" icon="simple-info" v-model="valueText" label="Input with icon" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
-          </div>
-
-          <div class="ec-col-4 ec-p--24">
-            <ec-input-field readonly placeholder="My input" value="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eos consequatur quas reiciendis aliquid ipsam ea pariatur dolorem, molestias maiores." label="Read only input with long text" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
-          </div>
-
-          <div class="ec-col-3 ec-p--24">
-            <ec-input-field placeholder="An other input" v-model="valueText" label="Input tooltip on the label" :is-in-group="isInGroup" :label-tooltip="labelTooltip" @change="onChange" @input="onInput" />
-          </div>
-          <div class="ec-col-3 ec-p--24">
-            <ec-input-field placeholder="My input" icon="simple-info" v-model="valueText" label="Short Label" :is-in-group="isInGroup" :label-tooltip="labelTooltip" :note="noteText" @change="onChange" @input="onInput" />
-          </div>
+    <div class="tw-grid-container">
+      <div class="tw-grid">
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field type="number" min="5" max="10" v-model.number="valueNumber" :note="noteNumber" :label="labelNumber" :error-message="errorMessageNumber" :icon="iconNumber" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
         </div>
 
-        <div class="ec-grid__row">
-          <div class="ec-col-3">
-            Model value number: {{ valueNumber }}
-          </div>
-          <div class="ec-col-4">
-            Model value text: {{ valueText }}
-          </div>
-          <div class="ec-col-5">
-            Model value date: {{ valueDate }}
-          </div>
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field type="text" placeholder="My input" v-model="valueText" :note="noteText" :label="labelText" :error-message="errorMessageText" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field type="date" placeholder="My input" v-model="valueDate" :note="noteDate" :label="labelDate" :error-message="errorMessageDate" :icon="iconDate" :is-in-group="isInGroup" @change="onChange" @input="onInput" />        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field disabled placeholder="My disabled input" v-model="valueText" label="Disabled input" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field disabled placeholder="My disabled input" v-model="valueText" label="Disabled input" error-message="Disabled with error" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field placeholder="My input" icon="simple-info" v-model="valueText" label="Input with icon" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field readonly placeholder="My input" value="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eos consequatur quas reiciendis aliquid ipsam ea pariatur dolorem, molestias maiores." label="Read only input with long text" :icon="iconText" :is-in-group="isInGroup" @change="onChange" @input="onInput" />
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field placeholder="An other input" v-model="valueText" label="Input tooltip on the label" :is-in-group="isInGroup" :label-tooltip="labelTooltip" @change="onChange" @input="onInput" />
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          <ec-input-field placeholder="My input" icon="simple-info" v-model="valueText" label="Short Label" :is-in-group="isInGroup" :label-tooltip="labelTooltip" :note="noteText" @change="onChange" @input="onInput" />
+        </div>
+
+        <div class="tw-col-full"></div>
+
+        <div class="tw-col-full md:tw-col-4">
+          Model value number: {{ valueNumber }}
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          Model value text: {{ valueText }}
+        </div>
+
+        <div class="tw-col-full md:tw-col-4">
+          Model value date: {{ valueDate }}
         </div>
       </div>
     </div>
