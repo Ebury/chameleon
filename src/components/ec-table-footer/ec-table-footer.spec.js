ad500cc2b76ae3fd457edd5012428e725cc7ba00
Fco. Javier Mesa Olmo <francisco.mesa@ebury.com>
2020-07-08 12:26:51 +0200

    ONL-4719:feat: Update table unit tests

--- src/components/ec-table-footer/ec-table-footer.spec.js
+++ src/components/ec-table-footer/ec-table-footer.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import EcTableFooter from './ec-table-footer.vue';
 
 describe('EcTableFooter', () => {
-  it('should render an empty row if no props are supplied', () => {
+  it('should render without footer slot', () => {
     const wrapper = mount(
       EcTableFooter,
     );
@@ -10,84 +10,12 @@ describe('EcTableFooter', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  it('should render with singular text if supplied with 1 item and no total items', () => {
+  it('should render with footer slot', () => {
     const wrapper = mount(
       EcTableFooter,
       {
-        propsData: {
-          itemsInView: 1,
-        },
-      },
-    );
-
-    expect(wrapper.element).toMatchSnapshot();
-  });
-
-  it('should render with the plural text when supplied with more than 1 item and no total items', () => {
-    const wrapper = mount(
-      EcTableFooter,
-      {
-        propsData: {
-          itemsInView: 2,
-        },
-      },
-    );
-
-    expect(wrapper.element).toMatchSnapshot();
-  });
-
-  it('should render with singular text if there is only 1 for total items', () => {
-    const wrapper = mount(
-      EcTableFooter,
-      {
-        propsData: {
-          itemsInView: 1,
-          totalItems: 1,
-        },
-      },
-    );
-
-    expect(wrapper.element).toMatchSnapshot();
-  });
-
-  it('should render without `total items` text if there are more items supplied than total items', () => {
-    const wrapper = mount(
-      EcTableFooter,
-      {
-        propsData: {
-          itemsInView: 13,
-          totalItems: 3,
-        },
-      },
-    );
-
-    expect(wrapper.element).toMatchSnapshot();
-  });
-
-  it('should render with `total items` text if there are more total items supplied than items in view', () => {
-    const wrapper = mount(
-      EcTableFooter,
-      {
-        propsData: {
-          itemsInView: 17,
-          totalItems: 21,
-        },
-      },
-    );
-
-    expect(wrapper.element).toMatchSnapshot();
-  });
-
-  it('should render with the icon of the tooltip if we pass the config', () => {
-    const wrapper = mount(
-      EcTableFooter,
-      {
-        propsData: {
-          tooltipConfig: {
-            content: 'This is the tooltip info',
-            classes: ['ec-tooltip--bg-bright'],
-            placement: 'bottom',
-          },
+        slots: {
+          default: '<p>Random text</p>',
         },
       },
     );
