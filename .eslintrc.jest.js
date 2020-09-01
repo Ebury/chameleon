1dfe3a45980400360d860704ddc8708e899c95db
Milos Cibulka <mcibique@users.noreply.github.com>
2020-09-01 13:49:55 +0100

    ONL-4163: style: New rules for jest eslint.

--- .eslintrc.jest.js
+++ .eslintrc.jest.js
@@ -2,4 +2,8 @@ module.exports = {
   extends: [
     'plugin:jest/recommended',
   ],
+  rules: {
+    'jest/no-standalone-expect': ['error', { additionalTestBlockFunctions: ['beforeEach', 'afterEach'] }],
+    'jest/expect-expect': ['error', { assertFunctionNames: ['expect', 'expect[A-Za-z]+'] }],
+  },
 };
