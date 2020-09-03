b03865cf4b58cc52ea32167116e6168ba5487456
Jose Fuentes <jose.fuentes@ebury.com>
2020-09-03 13:08:13 +0200

    feat(ONL-4824): PR comments

--- src/styles/generic/fonts.story.js
+++ src/styles/generic/fonts.story.js
@@ -37,15 +37,6 @@ stories.add('fonts', () => ({
 
     return (
       <div class={`tw-p-24 ${fontFamily}`}>
-        <p style={style}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales felis nec libero vehicula, sit amet gravida dolor cursus. Sed suscipit turpis eget blandit vestibulum. Quisque congue hendrerit metus, eget laoreet metus varius et. Aliquam euismod a lacus eu rhoncus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus in mi eu ligula feugiat aliquet. Duis imperdiet metus a semper tristique. Phasellus hendrerit massa at lectus porttitor, in maximus ligula ultricies. Aliquam sollicitudin dolor sed ligula interdum, et placerat neque consectetur. Vestibulum ut risus lorem.
-        </p>
-        <p style={style}>
-          Morbi ultrices pellentesque ex. Etiam efficitur dapibus urna, vitae sollicitudin lectus efficitur id. Sed porta sapien non augue fringilla, vel ultrices nulla vestibulum. In vehicula sem a lobortis malesuada. Curabitur eu auctor ligula. Phasellus et luctus ex. Vivamus semper magna ante, vel feugiat quam vulputate et. Sed eleifend ante non ligula malesuada, ut scelerisque eros interdum. Duis condimentum augue sit amet ligula imperdiet pharetra. Nulla eget metus orci. Cras auctor, ex ullamcorper auctor aliquam, sapien risus efficitur orci, sit amet varius turpis leo at enim. Nullam at ornare dui, quis lacinia nulla.
-        </p>
-        <p style={style}>
-          ABCČĆDĐEFGHIJKLMNOPQRSŠTUVWXYZŽabcčćdđefghijklmnopqrsštuvwxyzžАБВГҐДЂЕЁЄЖЗЅИІЇЙЈКЛЉМНЊОПРСТЋУЎФХЦЧЏШЩЪЫЬЭЮЯабвгґдђеёєжзѕиіїйјклљмнњопрстћуўфхцчџшщъыьэюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψωάΆέΈέΉίϊΐΊόΌύΰϋΎΫΏĂÂÊÔƠƯăâêôơư1234567890‘?’“!”(%)[#]@/&\-+÷×=®©$€£¥¢:;,.*
-        </p>
         <div class="tw-mini-header">English</div>
         <p style={style}>
           In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing.
@@ -101,6 +92,10 @@ stories.add('fonts', () => ({
         <div class="tw-mini-header">Greek</div>
         <p style={style}>
           Σε ένα χωριό La Mancha, το όνομα του οποίου δεν έχω καμία επιθυμία να θυμηθώ, δεν έζησε πολύς καιρός από έναν από αυτούς τους κυρίους που κρατούν μια λόγχη στο ράφι, έναν παλιό κάδο, ένα άπαχο χάκερ και ένα λαγωνικό για μαθήματα.
+        </p>
+        <div class="tw-mini-header">Special characters</div>
+        <p style={style}>
+          1234567890‘?’“!”(%)[#]@/&\-+÷×=®©$€£¥¢:;,.*
         </p>
       </div>
     );
