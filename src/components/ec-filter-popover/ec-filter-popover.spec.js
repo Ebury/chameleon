6933ac53525f7c19dcd3e63533b7b37d8520c1e1
MaruaEl <maroua.elbaoui@ebury.com>
2020-10-26 13:15:45 +0200

    ONL-4910 ecFilterPopover (#483)
    * add new ec filer popover component and story
    
    * updated snapshots
    
    * tweaks
    
    * updated snapshot
    
    * removed unused code
    
    * tweaked and remove non used code
    
    * add height of container
    
    * updated snapshot
    
    * updated snapshot
    
    * changes after pr review comments
    
    * removed emit to follow YAGNI pronciples
    
    * reverted package.json
    
    * added component in main.js
    
    * tweaked story to show props and arguments and attempt at fix for popover overflow
    
    * updated snapshot
    
    * revert package-lock.json
    
    * fixed popover overflow
    
    * add test for onOpen method in unit test and fix margin
    
    * fixed silly mistake in sotry v-model after Ive renamed property
    
    * fixed margin left
    
    * ONL-4910 create snapshots
    
    * added button type, fixed unit tests
    
    * 1.0.20
    
    Co-authored-by: Maroua El Baoui <marouaelbaoui@MacBook-Pro-Maroua-el-Baoui.local>
    Co-authored-by: jaimeperezpedra <jaimeperezpedra@gmail.com>
    Co-authored-by: Carlos Matallín Civera <36676+matallo@users.noreply.github.com>

---
+++ src/components/ec-filter-popover/ec-filter-popover.spec.js
@@ -0,0 +1,63 @@
+import { mount } from '@vue/test-utils';
+import EcFilterPopover from './ec-filter-popover.vue';
+import { withMockedConsole } from '../../../tests/utils/console';
+
+const label = 'Test label';
+const numberOfSelectedFilters = 0;
+
+function mountEcFilterPopover(props, mountOpts) {
+  return mount(EcFilterPopover, {
+    stubs: { EcPopover: true },
+    propsData: {
+      ...props,
+    },
+    ...mountOpts,
+  });
+}
+describe('EcFilterPopover', () => {
+  it('should throw an error if no label prop were given', () => {
+    withMockedConsole((errorSpy) => {
+      mountEcFilterPopover({ numberOfSelectedFilters });
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+      expect(errorSpy.mock.calls[0][0]).toContain('Missing required prop: "label"');
+    });
+  });
+
+  it('should render properly when label prop was given', () => {
+    const wrapper = mountEcFilterPopover({ label, numberOfSelectedFilters });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should throw an error if no numberOfSelectedFilters prop were given', () => {
+    withMockedConsole((errorSpy) => {
+      mountEcFilterPopover({ label });
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+      expect(errorSpy.mock.calls[0][0]).toContain('Missing required prop: "numberOfSelectedFilters"');
+    });
+  });
+
+  it('numberOfSelectedFilters should not be visible if it\'s value is less than 0', () => {
+    const wrapper = mountEcFilterPopover({ label, numberOfSelectedFilters });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('numberOfSelectedFilters should be visible if it\'s value is greater than 0 ', () => {
+    const wrapper = mountEcFilterPopover({ label, numberOfSelectedFilters: 5 });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should render correctly the named slot', () => {
+    const wrapper = mountEcFilterPopover({ label, numberOfSelectedFilters }, {
+      scopedSlots: {
+        filter: '<p>Test name slot</p>',
+      },
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should set the open status of the popover', async () => {
+    const wrapper = mountEcFilterPopover({ label, numberOfSelectedFilters });
+    await wrapper.find('ecpopover-stub').vm.$emit('update:open', true);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
---
+++ src/components/ec-filter-popover/ec-filter-popover.vue
@@ -0,0 +1,199 @@
+<template>
+  <div
+    ref="filterPopover"
+    class="ec-filter-popover"
+    data-test="ec-filter-popover"
+  >
+    <ec-popover
+      v-bind="getPopoverOptions"
+      :popper-options="popperOptions"
+      :placement="'bottom'"
+      offset="0, 8"
+      @update:open="onOpen"
+    >
+      <div
+        class="ec-filter-popover__trigger"
+        :class="{ 'ec-filter-popover__trigger--focused': triggerIsFocused }"
+        data-test="ec-filter-popover__trigger"
+      >
+        <button
+          type="button"
+          class="ec-filter-popover__label"
+          data-test="ec-filter-popover__label"
+        >{{ label }}</button>
+        <span
+          v-if="numberOfSelectedFilters > 0"
+          class="ec-filter-popover__badge"
+        > {{ numberOfSelectedFilters }}
+        </span>
+        <!-- <ec-badge-number size="20" value="numberOfSelectedFilters" />
+        TODO Badge with https://fxsolutions.atlassian.net/browse/ONL-4909 -->
+        <ec-icon
+          class="ec-filter-popover__icon"
+          :size="16"
+          name="simple-chevron-down"
+          type="interactive"
+        />
+      </div>
+
+      <div
+        slot="popover"
+        class="ec-filter-popover__filter-content"
+      >
+        <slot
+          name="filter"
+        />
+      </div>
+    </ec-popover>
+  </div>
+</template>
+<script>
+import EcPopover from '../ec-popover';
+import EcIcon from '../ec-icon';
+
+export default {
+  name: 'EcFilterPopover',
+  components: { EcPopover, EcIcon },
+  props: {
+    label: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    numberOfSelectedFilters: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    popoverOptions: {
+      type: Object,
+    },
+    popperModifiers: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      triggerIsFocused: false,
+      popperOptions: {
+        modifiers: {
+          calculatePopperPosition: {
+            // We need to check whether the popover is escaping from the boundaries of
+            // the boundariesElement and if so, fix the position of the popover.
+            // normally, we could just check the data.boundaries but there is a bug in popper.js v1.
+            // their docs says that the data.boundaries exists (https://popper.js.org/docs/v1/#dataobject) but
+            // they accidentaly assign that info into options of the preventOverflow modifier when
+            // calculating boundaries. see: https://github.com/popperjs/popper-core/blob/v1.16.1/packages/popper/src/modifiers/preventOverflow.js#L47
+            // we can do a workaround and access the boundaries object via this code:
+            // const preventOverflowOptions = data.instance.modifiers.find(m => m.name === 'preventOverflow');
+            // const popperBoundaries = preventOverflowOptions.boundaries;
+            enabled: true,
+            order: 845,
+            fn: /* istanbul ignore next */ (data) => {
+              const preventOverflowOptions = data.instance.modifiers.find(m => m.name === 'preventOverflow');
+              const popperBoundaries = preventOverflowOptions.boundaries;
+              const { left, right } = popperBoundaries;
+
+              let popperLeft = Math.max(data.offsets.reference.left, left);
+              let popperRight = popperLeft + data.popper.width;
+
+              if (popperRight > right) {
+                popperRight = right;
+                popperLeft = popperRight - data.popper.width;
+              }
+
+              data.offsets.popper.left = popperLeft;
+              data.popper.left = popperLeft;
+              data.offsets.popper.right = popperRight;
+              data.popper.right = popperRight;
+
+              return data;
+            },
+          },
+          ...this.popperModifiers,
+        },
+      },
+    };
+  },
+  computed: {
+    getPopoverOptions() {
+      return {
+        ...this.popoverOptions,
+      };
+    },
+  },
+  methods: {
+    onOpen(status) {
+      this.triggerIsFocused = status;
+    },
+  },
+};
+</script>
+<style>
+:root {
+  --ec-filter-popover-width: 304px;
+  --ec-filter-popover-height: 368px;
+}
+
+.ec-filter-popover {
+  &__trigger {
+    @apply tw-flex tw-flex-row tw-items-center;
+    @apply tw-p-8;
+    @apply tw-transition-all tw-duration-200 tw-ease-in-out;
+
+    &:focus {
+      @apply tw-bg-key-7;
+      @apply tw-rounded;
+      @apply tw-outline-none;
+    }
+
+    &--focused {
+      @apply tw-bg-key-7;
+      @apply tw-rounded;
+    }
+
+    &:hover {
+      @apply tw-bg-key-6;
+      @apply tw-rounded;
+    }
+  }
+
+  &__label {
+    @apply tw-p-0 tw-m-0;
+    @apply tw-border-none;
+    @apply tw-text-gray-3;
+    @apply tw-bg-transparent;
+
+    &:hover {
+      @apply tw-no-underline;
+    }
+
+    &:focus {
+      @apply tw-outline-none;
+    }
+  }
+
+  &__icon {
+    @apply tw-flex-shrink-0;
+    @apply tw-self-center;
+    @apply tw-ml-4;
+  }
+
+  &__badge {
+    /* TODO remove this styles once we have the badge component */
+    @apply tw-ml-4;
+    @apply tw-bg-key-5 tw-text-gray-8;
+    @apply tw-rounded-1/2;
+    @apply tw-w-20;
+    @apply tw-text-center tw-flags-text;
+  }
+
+  &__filter-content {
+    @apply tw-bg-gray-8;
+    @apply tw-border-gray-6 tw-border-solid tw-border tw-rounded;
+
+    width: var(--ec-filter-popover-width);
+    max-height: var(--ec-filter-popover-height);
+  }
+}
+</style>
