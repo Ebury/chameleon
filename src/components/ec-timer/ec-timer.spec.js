3f236cc062ed51dd0a093ba440c9128a8f2601a5
stavros <stavros.tomadakis@ebury.com>
2020-11-15 12:41:24 +0000

    ONL-5054:test: timer unit specs, unit snaps, integration snaps

--- src/components/ec-timer/ec-timer.spec.js
+++ src/components/ec-timer/ec-timer.spec.js
@@ -1,6 +1,39 @@
+import { mount } from '@vue/test-utils';
 import EcTimer from './ec-timer.vue';
+import { withMockedConsole } from '../../../tests/utils/console';
 
-export default {
-  title: 'Timer',
-  component: EcTimer,
-};
+describe('EcTimer', () => {
+  function mountTimer(props) {
+    return mount(EcTimer, {
+      propsData: {
+        ...props,
+      },
+    });
+  }
+
+  it('should throw an error if "seconds" prop is not given', () => {
+    withMockedConsole((errorSpy) => {
+      mount(EcTimer);
+
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+      expect(errorSpy.mock.calls[0][0]).toContain('Missing required prop: "seconds"');
+    });
+  });
+
+  it('should throw an error if "seconds" prop is not an integer', () => {
+    withMockedConsole((errorSpy) => {
+      mountTimer({ seconds: 20.1 });
+
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+      expect(errorSpy.mock.calls[0][0]).toContain('Invalid prop: custom validator check failed for prop "seconds"');
+    });
+  });
+
+  it('should render as expected', () => {
+    const wrapper = mountTimer({ seconds: 20 });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  // TODO: test that emits 'timer-complete' event after we add lolex or similar
+});
