24b83bb771eb45ddbb78e9a34f36d1a4c9dcff7d
Jose Calvente <jose.fuentes@ebury.com>
2020-11-17 18:21:58 +0100

    [ONL-5055] Updating snapshots

--- src/components/ec-currency-input/ec-currency-input.story.js
+++ src/components/ec-currency-input/ec-currency-input.story.js
@@ -2,6 +2,8 @@ import EcCurrencyInput from './ec-currency-input.vue';
 
 const bottomNote = 'On balance: EUR 300.00';
 const currencies = ['GBP', 'EUR', 'USD', 'CAD', 'JPY', 'AUD', 'NZD', 'CHF', 'SEK', 'NOK', 'AED', 'BBD', 'BGN', 'BHD', 'BSD', 'BWP', 'BZD', 'CNY', 'CZK', 'DKK', 'GHS', 'HKD', 'HRK', 'HUF', 'ILS', 'INR', 'JMD', 'KES', 'LKR', 'MUR', 'MXN', 'NGN', 'PHP', 'PKR', 'PLN', 'QAR', 'RON', 'RSD', 'RUB', 'SAR', 'SGD', 'SZL', 'THB', 'TND', 'TRY', 'TTD', 'UGX', 'XCD', 'ZAR', 'ZMW'];
+const label = 'Currency input';
+const note = 'Select currency and set amount';
 const warningMessage = 'Currently there is not enough EUR balance to cover the transaction amount, ensure you add funds to your balance after finishing.';
 
 export default {
@@ -37,5 +39,7 @@ basic.args = {
   bottomNote,
   currencies,
   isWarning: true,
+  label,
+  note,
   warningMessage,
 };
