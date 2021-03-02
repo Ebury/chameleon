f4980c8f9362e72139fa119bd53e4757f92071d2
jaimeperezpedra <jaimeperezpedra@gmail.com>
2021-03-02 12:05:50 +0000

    Onl 5574 countdown (#713)
    * ONL-5574 create the countdown
    
    * ONL-5574 add the export on the main.js
    
    * ONL-5574 create the coutdown on the data
    
    * ONL-5574 import instead of required
    
    * ONL-5574 change library to emmit events and create the stop event
    
    * ONL-5574 clear emits
    
    * ONl-5574 remove clear of the events
    
    * ONL-5574 check stop function
    
    * ONL-5574 assign the events directly to the class, ammend commets
    
    * ONL-5574 set countdown to null when the timer is not running
    
    * ONL-5574 remove unnecesary code
    
    * ONL-5574 reuse code in a method

---
+++ src/utils/countdown.spec.js
@@ -0,0 +1,47 @@
+import fakeTimers from '@sinonjs/fake-timers';
+import Countdown from './countdown';
+
+describe('Countdown', () => {
+  let clock;
+
+  beforeEach(() => {
+    clock = fakeTimers.install(window);
+  });
+  afterEach(() => {
+    if (clock) {
+      clock.uninstall();
+    }
+  });
+  it('should finish the countdown', () => {
+    const countdown = new Countdown();
+    countdown.start(20);
+    clock.tick(20000);
+
+    expect(countdown.secondsLeft).toBe(0);
+    expect(clock.countTimers()).toBe(0);
+  });
+
+  it('should update the "secondsLeft" when the time is running', () => {
+    const countdown = new Countdown();
+    countdown.start(20);
+    clock.tick(10000);
+    expect(countdown.secondsLeft).toBe(10);
+    clock.tick(10000);
+    expect(countdown.secondsLeft).toBe(0);
+    expect(clock.countTimers()).toBe(0);
+  });
+
+  it('should emit the events', () => {
+    const timeExpiredMock = jest.fn();
+    const timeUpdatedMock = jest.fn();
+    const countdown = new Countdown();
+    countdown.on('time-expired', timeExpiredMock);
+    countdown.on('time-updated', timeUpdatedMock);
+    countdown.start(20);
+    clock.tick(20000);
+
+    expect(timeExpiredMock).toBeCalledTimes(1);
+    expect(timeUpdatedMock).toBeCalledTimes(20);
+    expect(clock.countTimers()).toBe(0);
+  });
+});
