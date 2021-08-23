58cf239310e8eb9c223887f78806d51b3681f7db
Milos Cibulka <mcibique@users.noreply.github.com>
2021-08-23 16:03:34 +0100

    ONL-5955: fix: Applied fix from purgecss github repo when purgecss tries to purge css before postcss-nested gets chance to process the CSS.

--- postcss.config.js
+++ postcss.config.js
@@ -1,46 +1,51 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 const twStoryPath = require.resolve('./src/styles/utilities/tailwind.story.css');
 
 module.exports = ({ file }) => {
   const config = {
-    plugins: {
-      'postcss-import': {},
-      'postcss-mixins': {},
-      tailwindcss: require.resolve('./tailwind.config.js'),
-      'postcss-nested': {},
-    },
+    plugins: [
+      require('postcss-import'),
+      require('postcss-mixins'),
+      require('tailwindcss'),
+      require('postcss-nested'),
+    ],
   };
 
   // Don't purge the story for TailwindCSS, otherwise it won't be able to get the full list of available utility classes.
   const shouldPurgeFile = file !== twStoryPath;
-  const isProd = process.env.NODE_ENV === 'production';
+  // const isProd = process.env.NODE_ENV === 'production';
+  const isProd = true;
   if (isProd && shouldPurgeFile) {
     // see https://purgecss.com/guides/vue.html for reference
-    config.plugins['@fullhuman/postcss-purgecss'] = {
+    const purgecss = require('@fullhuman/postcss-purgecss')({
       content: [
         './src/**/*.vue',
         './src/**/*.story.js',
         './src/**/*.svg',
-        './.storybook/*.js'],
+        './.storybook/*.js',
+      ],
       defaultExtractor(content) {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
       },
-      safelist: {
-        standard: [
-          /-(leave|enter|appear)(|-(to|from|active))$/,
-          /^(?!(|.*?:)cursor-move).+-move$/,
-          /^router-link(|-exact)-active$/,
-          /data-v-.*/,
-          /^ec-/,
-        ],
-        deep: [
-          /^ec-/,
-        ],
-        greedy: [],
-        keyframes: [],
-        variables: [],
-      },
-    };
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+        /^ec-/,
+      ],
+    });
+
+    if (purgecss.Once && !purgecss.OnceExit) {
+      // apply the fix for interacting with postcss-nested from PR: https://github.com/FullHuman/purgecss/pull/647
+      // this PR has been merged in April 2021 but still hasn't released in August!!!
+      purgecss.OnceExit = purgecss.Once;
+      delete purgecss.Once;
+    }
+
+    config.plugins.push(purgecss);
   }
 
   return config;
