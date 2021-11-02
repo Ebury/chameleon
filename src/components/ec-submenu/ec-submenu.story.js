d9f06e4b8a31c7bd56bd65a9dcb2faca7fe6d00d
Milos Cibulka <mcibique@users.noreply.github.com>
2021-11-02 12:01:36 +0000

    ONL-4915: fix: Fixed story for Submenu not using the onChanged method.

--- src/components/ec-submenu/ec-submenu.story.js
+++ src/components/ec-submenu/ec-submenu.story.js
@@ -20,7 +20,7 @@ const Template = (args, { argTypes }) => ({
   },
   template: `
     <div class="tw-m-24">
-      <ec-submenu v-bind="$props" v-model="activeIndex">
+      <ec-submenu v-bind="$props" v-model="activeIndex" @change="onChanged">
         <template #menu-item-1-content>
           <h1> One - 1 </h1>
           <p>lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, tempora esse? Fugit minus consequatur harum culpa sed laborum nulla expedita molestias, quia cupiditate commodi repudiandae labore dolore dolor. Eum sapiente repellendus tempora tenetur accusantium, molestias hic adipisci aspernatur, sed possimus explicabo culpa quas laudantium? Quidem sunt totam maxime magni minus dignissimos eos libero nobis accusantium aspernatur qui inventore odio adipisci at dolorum consectetur velit ipsa quia assumenda porro, consequatur nisi mollitia deserunt. Iusto possimus debitis assumenda harum atque libero voluptates fugit quibusdam ipsa voluptatibus eligendi ex doloremque ducimus molestias natus adipisci, earum facere repudiandae sint nobis error maiores iste. Quam.</p>
