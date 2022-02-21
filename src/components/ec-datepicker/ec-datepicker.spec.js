97ec8d5a290b652dbb6e38836bda7f42ae1bb7d7
stavros <stavros.tomadakis@ebury.com>
2022-02-21 18:51:40 +0000

    ONL-6337:test:unit

--- src/components/ec-datepicker/ec-datepicker.spec.js
+++ src/components/ec-datepicker/ec-datepicker.spec.js
@@ -1,76 +1,185 @@
-import { mount } from '@vue/test-utils';
+import {
+  mount,
+  createWrapper,
+  createLocalVue,
+} from '@vue/test-utils';
 import EcDatepicker from './ec-datepicker.vue';
 
 describe('Datepicker', () => {
   function mountDatepicker(props, mountOpts) {
-    return mount(EcDatepicker, {
+    const inputWrapper = mount(EcDatepicker, {
       propsData: { ...props },
       ...mountOpts,
     });
+
+    const calendarWrapper = createWrapper(document.body).findByDataTest('ec-datepicker__calendar');
+
+    return { inputWrapper, calendarWrapper };
+  }
+
+  function mountDatepickerAsTemplate(template, props, wrapperComponentOpts, mountOpts) {
+    const localVue = createLocalVue();
+
+    const Component = localVue.extend({
+      components: { EcDatepicker },
+      template,
+      ...wrapperComponentOpts,
+    });
+
+    const inputWrapper = mount(Component, {
+      propsData: { ...props },
+      ...mountOpts,
+    });
+
+    const calendarWrapper = createWrapper(document.body).findByDataTest('ec-datepicker__calendar');
+
+    return { inputWrapper, calendarWrapper };
   }
 
   it('should render properly', () => {
-    const wrapper = mountDatepicker();
+    const { inputWrapper } = mountDatepicker();
 
-    expect(wrapper.element).toMatchSnapshot();
+    expect(inputWrapper.element).toMatchSnapshot();
   });
 
   describe(':props', () => {
     it('should render with a label', () => {
-      const wrapper = mountDatepicker({
+      const { inputWrapper } = mountDatepicker({
         label: 'Datepicker label',
       });
 
-      expect(wrapper.element).toMatchSnapshot();
+      expect(inputWrapper.element).toMatchSnapshot();
     });
 
     it('should render with a note', () => {
-      const wrapper = mountDatepicker({
+      const { inputWrapper } = mountDatepicker({
         note: 'Datepicker note',
       });
 
-      expect(wrapper.element).toMatchSnapshot();
+      expect(inputWrapper.element).toMatchSnapshot();
     });
 
     it('should render with a bottom note', () => {
-      const wrapper = mountDatepicker({
+      const { inputWrapper } = mountDatepicker({
         bottomNote: 'Datepicker bottom note',
       });
 
-      expect(wrapper.element).toMatchSnapshot();
+      expect(inputWrapper.element).toMatchSnapshot();
     });
 
     it('should render with a bottom note in a warning state', () => {
-      const wrapper = mountDatepicker({
+      const { inputWrapper } = mountDatepicker({
         bottomNote: 'Datepicker bottom note',
         isWarning: true,
       });
 
-      expect(wrapper.element).toMatchSnapshot();
+      expect(inputWrapper.element).toMatchSnapshot();
     });
 
     it('should render with a placeholder', () => {
-      const wrapper = mountDatepicker({
+      const { inputWrapper } = mountDatepicker({
         placeholder: 'Datepicker placeholder',
       });
 
-      expect(wrapper.element).toMatchSnapshot();
+      expect(inputWrapper.element).toMatchSnapshot();
     });
 
     it('should render with an error message', () => {
-      const wrapper = mountDatepicker({
+      const { inputWrapper } = mountDatepicker({
         errorMessage: 'A random error message',
       });
 
-      expect(wrapper.element).toMatchSnapshot();
+      expect(inputWrapper.element).toMatchSnapshot();
     });
 
     it('should render as disabled', () => {
-      const wrapper = mountDatepicker({
+      const { inputWrapper } = mountDatepicker({
         isDisabled: true,
       });
 
-      expect(wrapper.element).toMatchSnapshot();
+      expect(inputWrapper.element).toMatchSnapshot();
     });
+
+    it('should have a disabled date', () => {
+      const { calendarWrapper } = mountDatepicker({
+        disabledDates: {
+          '2022-02-22': '',
+        },
+      });
+
+      expect(calendarWrapper.findByDataTest('ec-datepicker__calendar-day--2022-02-22').classes('flatpickr-disabled')).toBe(true);
+
+      expect(calendarWrapper.element).toMatchSnapshot('calendar');
+    });
+
+    it('should have a disabled weekends', () => {
+      const { calendarWrapper } = mountDatepicker({
+        areWeekendsDisabled: true,
+      });
+
+      expect(calendarWrapper.findByDataTest('ec-datepicker__calendar-day--2022-02-19').classes('flatpickr-disabled')).toBe(true);
+      expect(calendarWrapper.findByDataTest('ec-datepicker__calendar-day--2022-02-20').classes('flatpickr-disabled')).toBe(true);
+
+      expect(calendarWrapper.element).toMatchSnapshot('calendar');
+    });
+  });
+
+  describe('v-model', () => {
+    it('should have preselected default date', () => {
+      const { inputWrapper } = mountDatepickerAsTemplate(
+        '<ec-datepicker v-model="model" />',
+        {},
+        {
+          data() {
+            return {
+              model: '2022-02-22',
+            };
+          },
+        },
+      );
+
+      expect(inputWrapper.findByDataTest('ec-input-field__input').element.value).toBe('2022-02-22');
+    });
+
+    it('should update the value of the calendar when I type a value', () => {
+      const { inputWrapper } = mountDatepickerAsTemplate(
+        '<ec-datepicker v-model="model" />',
+        {},
+        {
+          data() {
+            return {
+              model: '',
+            };
+          },
+        },
+      );
+
+      inputWrapper.findByDataTest('ec-input-field__input').setValue('2022-02-23');
+
+      expect(inputWrapper.findByDataTest('ec-input-field__input').element.value).toBe('2022-02-23');
+    });
+
+    it('should update the value of the calendar when I select a value from the datepicker', () => {
+      const { inputWrapper, calendarWrapper } = mountDatepickerAsTemplate(
+        '<ec-datepicker v-model="model" />',
+        {},
+        {
+          data() {
+            return {
+              model: '',
+            };
+          },
+        },
+      );
+
+      calendarWrapper
+        .findByDataTest('ec-datepicker__calendar-day--2022-02-24')
+        .trigger('click');
+
+      expect(inputWrapper.findByDataTest('ec-input-field__input').element.value).toBe('2022-02-24');
+    });
+
+    // TODO test min date
+    // TODO test max date
   });
 });
