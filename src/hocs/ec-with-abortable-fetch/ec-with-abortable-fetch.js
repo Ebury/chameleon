acd732f64b968413941bc54afa8b7d6f917cc63c
Jose Fuentes <jafcalvente@outlook.com>
2022-03-03 10:37:22 +0100

    [ONL-6386] Removing finally block in EcWithAbortableFetch (#1079)
    * [ONL-6386] Removing finally block in EcWithAbortableFetch
    
    * 1.9.7
    
    Co-authored-by: Jose Calvente <jose.fuentes@ebury.com>

--- src/hocs/ec-with-abortable-fetch/ec-with-abortable-fetch.js
+++ src/hocs/ec-with-abortable-fetch/ec-with-abortable-fetch.js
@@ -56,14 +56,15 @@ const withAbortableFetch = (Component, {
       this.fetchAbortController = new global.AbortController();
       try {
         this.fetchedData = await this.dataSource.fetch(fetchArgs, this.fetchAbortController.signal);
+        this.isFetching = false;
+        this.fetchAbortController = null;
       } catch (err) {
         if (err && err.name !== 'AbortError') {
           this.$emit('error', err);
           this.fetchError = err;
+          this.isFetching = false;
+          this.fetchAbortController = null;
         }
-      } finally {
-        this.fetchAbortController = null;
-        this.isFetching = false;
       }
     },
   },
