3b4d5f76e544dec6a33e5d4ea49dbf2e54306ff4
Milos Cibulka <mcibique@users.noreply.github.com>
2022-06-08 22:44:47 +0100

    ONL-6573: feat: Migrate ec-loading-icon to Vue 3, updated tests and story.

--- src/components/ec-loading-icon/ec-loading-icon.story.js
+++ src/components/ec-loading-icon/ec-loading-icon.story.js
@@ -5,12 +5,14 @@ export default {
   component: EcLoadingIcon,
 };
 
-export const basic = (args, { argTypes }) => ({
+export const basic = args => ({
   components: { EcLoadingIcon },
-  props: Object.keys(argTypes),
+  setup() {
+    return { args };
+  },
   template: `
     <div class="tw-p-24 tw-flex tw-justify-center">
-      <ec-loading-icon v-bind="$props" />
+      <ec-loading-icon v-bind="args" />
     </div>
   `,
 });
