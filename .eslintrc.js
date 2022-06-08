11abccab9a8d33dd72352ba1b9ca947ced303c31
Milos Cibulka <mcibique@users.noreply.github.com>
2022-06-08 22:14:59 +0100

    ONL-6579: feat: Enabled Vue 3 rules for eslint.
    * Added vuejs-accessibility plugin for eslint required by @vue/eslint-config-airbnb. Disabled most their rules because they require refactoring in components. We'll do that after migration of Vue 3.
    * Disabled import/first rule for vue files because it breaks multiple <script> blocks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     parser: '@babel/eslint-parser',
   },
   extends: [
-    'plugin:vue/recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
   ],
   rules: {
@@ -19,11 +19,12 @@ module.exports = {
     'no-param-reassign': ['error', { props: false }],
     'no-plusplus': 'off', // we don't need this rule, we DO use semicolons!
     'no-restricted-syntax': 'off',
+    'no-restricted-exports': 'off',
     'no-underscore-dangle': ['error'],
     'no-use-before-define': 'off',
     'no-multiple-empty-lines': ['error', { max: 1 }],
-    'import/prefer-default-export': 'off',
     'require-await': ['error'],
+    'import/prefer-default-export': 'off',
     'vue/attributes-order': ['error', {
       order: [
         'GLOBAL',
@@ -40,12 +41,20 @@ module.exports = {
       ],
     }],
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/multi-word-component-names': 'off',
     'vue/multiline-html-element-content-newline': 'off',
     'vue/require-default-prop': 'off',
     'vue/singleline-html-element-content-newline': 'off',
+    'vue/one-component-per-file': 'off',
     // vue/compat
     'vue/no-deprecated-dollar-listeners-api': 'off',
     'vue/no-deprecated-v-on-native-modifier': 'off',
+    // TODO:
+    'vuejs-accessibility/label-has-for': 'off',
+    'vuejs-accessibility/click-events-have-key-events': 'off',
+    'vuejs-accessibility/anchor-has-content': 'off',
+    'vuejs-accessibility/form-control-has-label': 'off',
+    'vuejs-accessibility/mouse-events-have-key-events': 'off',
   },
   overrides: [
     {
@@ -57,6 +66,7 @@ module.exports = {
       },
       rules: {
         'global-require': 'off',
+        'vue/require-prop-types': 'off',
       },
     },
     {
@@ -66,6 +76,14 @@ module.exports = {
       rules: {
         'import/no-extraneous-dependencies': 'off',
         'vue/require-prop-types': 'off',
+      },
+    },
+    {
+      files: [
+        '**/*.vue',
+      ],
+      rules: {
+        'import/first': 'off',
       },
     },
   ],
