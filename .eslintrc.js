04196f76dc40cf127572254c7dcc4378bf361333
Milos Cibulka <mcibique@users.noreply.github.com>
2022-07-16 19:14:14 +0100

    [ONL6746] chore: Updated lint rules needed by EBO. Reordered rules alphabetically.

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,16 +14,17 @@ module.exports = {
   plugins: ['simple-import-sort'],
   rules: {
     'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'default-param-last': 'off',
     'max-len': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-multiple-empty-lines': ['error', { max: 1 }],
     'no-param-reassign': ['error', { props: false }],
     'no-plusplus': 'off', // we don't need this rule, we DO use semicolons!
-    'no-restricted-syntax': 'off',
     'no-restricted-exports': 'off',
+    'no-restricted-syntax': 'off',
     'no-underscore-dangle': ['error'],
     'no-use-before-define': 'off',
-    'no-multiple-empty-lines': ['error', { max: 1 }],
     'require-await': ['error'],
     'import/prefer-default-export': 'off',
     'simple-import-sort/imports': ['error', {
@@ -53,17 +54,18 @@ module.exports = {
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
     'vue/multi-word-component-names': 'off',
     'vue/multiline-html-element-content-newline': 'off',
+    'vue/one-component-per-file': 'off',
     'vue/require-default-prop': 'off',
     'vue/singleline-html-element-content-newline': 'off',
-    'vue/one-component-per-file': 'off',
     // vue/compat
     'vue/no-deprecated-dollar-listeners-api': 'off',
     'vue/no-deprecated-v-on-native-modifier': 'off',
+    'vue/no-deprecated-filter': 'off',
     // TODO:
-    'vuejs-accessibility/label-has-for': 'off',
-    'vuejs-accessibility/click-events-have-key-events': 'off',
     'vuejs-accessibility/anchor-has-content': 'off',
+    'vuejs-accessibility/click-events-have-key-events': 'off',
     'vuejs-accessibility/form-control-has-label': 'off',
+    'vuejs-accessibility/label-has-for': 'off',
     'vuejs-accessibility/mouse-events-have-key-events': 'off',
   },
   overrides: [
