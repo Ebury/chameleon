72959fe8e3ba8ba01cb4706920110b247ab1f66a
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-09-14 11:47:53 +0200

    Onl 4909 create ec badge component (#1276)
    * [ONL-4909] - created empty component
    
    * [ONL-4909] - added template and props
    
    * [ONL-4909] - dev - added export
    
    * [ONL-4909] - fix - badge style
    
    * [ONL-4909] - ref - changed badge values
    
    * [ONL-4909] - ref - using default slot
    
    * [ONL-4909] - dev - default slot
    
    * [ONL-4909] - ref - rm unecessary import
    
    * [ONL-4909] - dev -  unit tests
    
    * [ONL-4909] - ref - changing css class
    
    * [ONL-4909] - introducing ec-badge component into ec-filter-popover
    
    * [ONL-4909] - dev - changed prop type
    
    * [ONL-4909] - dev - adapting tests
    
    * dev - update story book examples
    
    * [ONL-4909] - ref - update snapshots
    
    * [ONL-4909] - dev - updated unit tests
    
    * [ONL-4909] - dev - added a type check test suite
    
    * [ONL-4909] -  ref changed indentation
    
    * [ONL-4909] -  test - visual-regression
    
    * [ONL-4909] - dev - updated  visual regression snapshots
    
    * [ONL-4909] - ref - rm comment
    
    * [ONL-4909] - fix - changed badge info bg
    
    * [ONL-4909] - ref - replaced snapshots
    
    * 2.0.8

---
+++ src/components/ec-badge/ec-badge.story.js
@@ -0,0 +1,90 @@
+import { reactive } from 'vue';
+
+import EcBadge from './ec-badge.vue';
+
+export default {
+  title: 'Badge',
+  component: EcBadge,
+  argTypes: {
+    type: {
+      options: ['error', 'info', 'success', 'warning'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+const Template = args => ({
+  components: { EcBadge },
+  setup() {
+    return {
+      args,
+    };
+  },
+  template: `
+    <ec-badge
+      v-bind="args"
+      class="tw-m-8" 
+    />
+  `,
+});
+
+export const basic = Template.bind({});
+
+basic.args = {
+  value: '1',
+};
+
+export const all = args => ({
+  components: { EcBadge },
+  setup() {
+    const badges = reactive([
+      {
+        title: 'Simple text',
+        data: [
+          { value: 'Info Badge', type: 'info' },
+          { value: 'Success Badge', type: 'success' },
+          { value: 'Warning Badge', type: 'warning' },
+          { value: 'Error Badge', type: 'error' },
+        ],
+      },
+      {
+        title: 'Simple number',
+        data: [
+          { value: 1, type: 'info' },
+          { value: 10, type: 'success' },
+          { value: 100, type: 'warning' },
+          { value: 1000, type: 'error' },
+        ],
+      },
+      {
+        title: 'Default slot',
+        data: [
+          { value: 'Info Badge', type: 'info', custom: true },
+          { value: 'Success Badge', type: 'success', custom: true },
+          { value: 'Warning Badge', type: 'warning', custom: true },
+          { value: 'Error Badge', type: 'error', custom: true },
+        ],
+      },
+    ]);
+
+    return {
+      args,
+      badges,
+    };
+  },
+  template: `
+    <div class="tw-m-16">
+      <template v-for="(block, blockIndex) in badges" :key="blockIndex">
+        <h3 class="tw-m-8">{{ block.title }}</h3>
+        <ec-badge 
+          v-for="(badge, badgeIndex) in block.data" 
+          v-bind="badge" 
+          :key="blockIndex + '-' + badgeIndex" 
+          class="tw-m-8">
+          <template v-if="badge.custom" #default="{ value }">   
+            Custom: {{ value }}
+          </template>
+        </ec-badge>
+      </template>
+    </div>`,
+});
