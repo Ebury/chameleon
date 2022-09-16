755bfa67c680cab5a1e5504db4a4c9d4dbb66c11
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-09-16 09:49:51 +0200

    [ONL-6862] - ref - rm log

--- src/components/ec-btn-dropdown/ec-btn-dropdown.spec.js
+++ src/components/ec-btn-dropdown/ec-btn-dropdown.spec.js
@@ -3,7 +3,6 @@ import { mount } from '@vue/test-utils';
 import EcBtnDropdown from './ec-btn-dropdown.vue';
 
 describe('EcBtnDropdown', () => {
-  console.log('prova');
   function mountBtnDropdown(props, mountOpts) {
     return mount(EcBtnDropdown, {
       props: {
