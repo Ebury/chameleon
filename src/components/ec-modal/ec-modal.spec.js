6ccae8f5961393405237336b0ede0e0a20d083bd
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-10-06 16:26:31 +0200

    [ONL-6575] - Replace focus trap directive (#1308)
    * [ONL-6575] - dev - added focus trapcomposable
    
    * [ONL-6575] - rm - custom directive from ec-modal
    
    * [ONL-6575] - del ec-focus-trap directive
    
    * [ONL-6575] - rm - ec-focus-trap mock
    
    * [ONL-6575] - ref - rm export
    
    * [ONL-6575] - dev - changed accepted module extension
    
    * [ONL-6575] - dev - changed focusTrap activation logic
    
    * [ONL-6575] - dev - adjusted mocking fn
    
    * [ONL-6575] - added new tests cases
    
    * [ONL-6575] - dev - updated tests
    
    * [ONL-6575] - rm failing test
    
    * [ONL-6575] - ref - updated new tests
    
    * [ONL-6575] - dev - added second modal tests
    
    * [ONL-6575] - ref - removed unecessary test case
    
    * [ONL-6575] - ref - rm unecessary passed params
    
    * [ONL-6575] - dev - reset inline options
    
    * [ONL-6575] - added useFocusTrap global mock
    
    * [ONL-6575] - ref - renamed file
    
    * 2.1.11

--- src/components/ec-modal/ec-modal.spec.js
+++ src/components/ec-modal/ec-modal.spec.js
@@ -1,10 +1,9 @@
 import { mount } from '@vue/test-utils';
+import { useFocusTrap } from '@vueuse/integrations/useFocusTrap';
 import { defineComponent } from 'vue';
 
 import { withMockedConsole } from '../../../tests/utils/console';
 import EcModal from './ec-modal.vue';
-
-jest.mock('../../directives/ec-focus-trap');
 
 function mountModal(props, mountOpts) {
   return mount(EcModal, {
@@ -31,6 +30,17 @@ describe('EcModal', () => {
     const wrapper = mountModal();
     expect(wrapper.findByDataTest('ec-modal').exists()).toBe(false);
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should be called with the "useFocusTrap composable" mandatory options', () => {
+    mountModal({ show: true });
+    const options = useFocusTrap.mock.calls[0][1];
+    expect(options)
+      .toEqual({
+        clickOutsideDeactivates: true,
+        escapeDeactivates: true,
+        immediate: true,
+      });
   });
 
   it('should render basic modal', () => {
@@ -386,6 +396,119 @@ describe('EcModal', () => {
 
       expect(wrapper.findByDataTest('ec-modal').exists()).toBe(false);
       expect(wrapper.element).toMatchSnapshot();
+    });
+  });
+
+  describe('when second modal is present', () => {
+    it('should be rendered with a second modal', () => {
+      const template = `
+      <div>
+        <ec-modal
+          is-closable
+          large
+          v-model:show="showFirstModal">
+  
+          <template #header>
+            <h2>First Modal</h2>
+          </template>
+  
+          <template #main>
+            <div>
+              <button data-test="first-modal-action__btn">Action first modal</button>
+              <p class="tw-mt-0">Before we can process your application we need you to upload your management accounts. You can do this now or leave it for later.</p>
+            </div>
+          </template>
+        </ec-modal>
+  
+        <ec-modal
+          is-closable
+          :z-index="zIndex"
+          v-model:show="showSecondModal">
+  
+          <template #header>
+            <h2>Second Modal</h2>
+          </template>
+  
+          <template #main>
+            <div>
+              <button data-test="second-modal-action__btn">Action second modal</button>
+              <p class="tw-mt-0">Before we can process your application we need you to upload your management accounts. You can do this now or leave it for later.</p>
+            </div>
+          </template>
+        </ec-modal>
+      </div>
+    `;
+      const wrapper = mountModalAsTemplate(
+        template,
+        {},
+        {
+          data() {
+            return {
+              showFirstModal: true,
+              showSecondModal: true,
+              zIndex: 201,
+            };
+          },
+        },
+      );
+      expect(wrapper.findByDataTest('ec-modal').element).toMatchSnapshot();
+      expect(wrapper.findByDataTest('second-modal-action__btn').exists()).toBe(true);
+    });
+    it('should be rendered with a second modal button clickable', () => {
+      const template = `
+      <div>
+        <ec-modal
+          is-closable
+          large
+          v-model:show="showFirstModal">
+  
+          <template #header>
+            <h2>First Modal</h2>
+          </template>
+  
+          <template #main>
+            <div>
+              <button @click.prevent.stop="firstModalAction" data-test="first-modal-action__btn">Action first modal</button>
+              <p class="tw-mt-0">Before we can process your application we need you to upload your management accounts. You can do this now or leave it for later.</p>
+            </div>
+          </template>
+        </ec-modal>
+  
+        <ec-modal
+          is-closable
+          :z-index="zIndex"
+          v-model:show="showSecondModal">
+  
+          <template #header>
+            <h2>Second Modal</h2>
+          </template>
+  
+          <template #main>
+            <div>
+              <button @click.prevent.stop="secondModalAction" data-test="second-modal-action__btn">Action second modal</button>
+              <p class="tw-mt-0">Before we can process your application we need you to upload your management accounts. You can do this now or leave it for later.</p>
+            </div>
+          </template>
+        </ec-modal>
+      </div>
+    `;
+      const wrapper = mountModalAsTemplate(
+        template,
+        {},
+        {
+          data() {
+            return {
+              showFirstModal: true,
+              showSecondModal: true,
+              zIndex: 201,
+              firstModalAction: jest.fn(),
+              secondModalAction: jest.fn(),
+            };
+          },
+        },
+      );
+      wrapper.findByDataTest('second-modal-action__btn').trigger('click');
+      expect(wrapper.vm.secondModalAction).toHaveBeenCalled();
     });
   });
 });
