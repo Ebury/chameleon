e3568ca128a6130c8058b4c4bf2f7d1e5dd77370
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-10-14 13:39:12 +0200

    [ONL-6942] - Fix table header text color (#1311)
    * [ONL-6942] - fix - applied correct color style
    
    * [ONL-6942] - ref - undo changes
    
    * [ONL-6942] - fix - applied correct color style

--- src/components/ec-table-head/ec-table-head.vue
+++ src/components/ec-table-head/ec-table-head.vue
@@ -126,6 +126,7 @@ function getStickyColumnClass(colIndex, columns) {
     @apply tw-z-level-1;
     @apply tw-table-header;
     @apply tw-min-w-104;
+    @apply tw-text-gray-3;
 
     box-shadow:
       inset 0 1px 0 theme('colors.gray.6'),
