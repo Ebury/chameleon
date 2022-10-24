1252dc08d3f1d53055e12272c0135233291b7d7e
corinaper <corina.perjan@ebury.com>
2022-10-24 14:40:04 +0200

    ONL-6979- Migrate EcSyncMultipleValuesFilter component

--- src/components/ec-sync-multiple-values-filter/ec-sync-multiple-values-filter.vue
+++ src/components/ec-sync-multiple-values-filter/ec-sync-multiple-values-filter.vue
@@ -29,110 +29,106 @@
     </ec-filter-popover>
   </div>
 </template>
-<script>
+
+<script setup>
+import { computed, ref } from 'vue';
+
 import { removeDiacritics } from '../../utils/diacritics';
 import EcFilterPopover from '../ec-filter-popover';
 import EcMultipleValuesSelection from '../ec-multiple-values-selection';
 
-export default {
-  name: 'EcSyncMultipleValuesFilter',
-  components: { EcFilterPopover, EcMultipleValuesSelection },
-  props: {
-    label: {
-      type: String,
-      required: true,
-      default: '',
-    },
-    modelValue: {
-      type: Array,
-      required: false,
-      default: () => ([]),
-    },
-    items: {
-      type: Array,
-      required: true,
-      default: () => ([]),
-    },
-    isLoading: {
-      type: Boolean,
-      default: false,
-    },
-    errorMessage: {
-      type: String,
-    },
-    emptyMessage: {
-      type: String,
-      default: 'No results found',
-    },
-    isSelectAll: {
-      type: Boolean,
-      default: false,
-    },
-    isSearchable: {
-      type: Boolean,
-      default: false,
-    },
-    selectAllFiltersText: {
-      type: String,
-      default: 'Select all',
-    },
-    popoverOptions: {
-      type: Object,
-    },
-    searchFilterPlaceholder: {
-      type: String,
-      default: 'Search...',
-    },
-    isFullHeight: {
-      type: Boolean,
-      default: false,
-    },
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+    default: '',
   },
-  emits: ['update:modelValue', 'change'],
-  data() {
-    return {
-      query: '',
-    };
+  modelValue: {
+    type: Array,
+    required: false,
+    default: () => ([]),
   },
-  computed: {
-    numberOfSelectedFilters() {
-      return this.selectedFilters.length;
-    },
-    selectedFilters: {
-      get() {
-        return this.modelValue;
-      },
-      set(value) {
-        this.$emit('update:modelValue', value);
-        this.$emit('change', value);
-      },
-    },
-    filteredItems() {
-      if (!this.isSearchable) {
-        return this.items;
-      }
+  items: {
+    type: Array,
+    required: true,
+    default: () => ([]),
+  },
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+  errorMessage: {
+    type: String,
+  },
+  emptyMessage: {
+    type: String,
+    default: 'No results found',
+  },
+  isSelectAll: {
+    type: Boolean,
+    default: false,
+  },
+  isSearchable: {
+    type: Boolean,
+    default: false,
+  },
+  selectAllFiltersText: {
+    type: String,
+    default: 'Select all',
+  },
+  popoverOptions: {
+    type: Object,
+  },
+  searchFilterPlaceholder: {
+    type: String,
+    default: 'Search...',
+  },
+  isFullHeight: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits(['update:modelValue', 'change']);
 
-      const filterText = removeDiacritics(this.query.toLowerCase());
-      if (!filterText) {
-        return this.items;
-      }
+// item(s) selection
+const selectedFilters = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit('update:modelValue', value);
+    emit('change', value);
+  },
+});
+const numberOfSelectedFilters = computed(() => selectedFilters.value.length);
 
-      return this.items.filter((item) => {
-        const itemText = removeDiacritics(item.text.trim().toLowerCase());
-        return itemText.includes(filterText);
-      });
-    },
-    emptyMessageText() {
-      return this.filteredItems.length ? null : this.emptyMessage;
-    },
-  },
-  methods: {
-    onSearch(query) {
-      this.query = query;
-    },
-    onPopoverOpened() {
-      this.$refs.multipleValuesSelection.focus();
-    },
-  },
-};
+// items filtering based on search query
+const searchQuery = ref('');
+function onSearch(newQuery) {
+  searchQuery.value = newQuery;
+}
+
+const filteredItems = computed(() => {
+  if (!props.isSearchable) {
+    return props.items;
+  }
+
+  const filterText = removeDiacritics(searchQuery.value.toLowerCase());
+  if (!filterText) {
+    return props.items;
+  }
+
+  return props.items.filter((item) => {
+    const itemText = removeDiacritics(item.text.trim().toLowerCase());
+    return itemText.includes(filterText);
+  });
+});
+
+const emptyMessageText = computed(() => (filteredItems.value.length ? null : props.emptyMessage));
+
+// focus when opened
+const multipleValuesSelection = ref(null);
+function onPopoverOpened() {
+  multipleValuesSelection.value.focus();
+}
 </script>
