24fd15b954a8e2ee721b162e0b9032ffd028b3bd
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-11-17 17:59:18 +0100

    [ONL-7025] - fix - reverted unecessary changes

--- tests/integration/specs/visual-regression/visual-regression.spec.js
+++ tests/integration/specs/visual-regression/visual-regression.spec.js
@@ -14,7 +14,7 @@ describe('Visual regression tests', () => {
     cy.visit('/');
     // wait for storybook to initialize. initialization is done when left menu is loaded
     // we can check it by waiting on menu items.
-    cy.get('a.sidebar-item', { timeout: 60000 });
+    cy.get('a.sidebar-item');
 
     cy.window()
       .then((win) => {
