0676d900e53e9dd4c53b7cdc0142e407e6a5e055
stavros <stavros.tomadakis@ebury.com>
2022-12-01 15:10:16 +0000

    [ONL-7052] chore: remove value

--- src/components/ec-datepicker/ec-datepicker.vue
+++ src/components/ec-datepicker/ec-datepicker.vue
@@ -198,7 +198,7 @@ let isTimeoutRunning = false;
 // In case the datepicker is open and the user scrolls we need to reposition the datepicker otherwise will appear detached from the input.
 /* c8 ignore start */
 function debouncedFlatpickrReposition() {
-  if (!isTimeoutRunning.value) {
+  if (!isTimeoutRunning) {
     isTimeoutRunning = true;
     setTimeout(() => {
       isTimeoutRunning = false;
