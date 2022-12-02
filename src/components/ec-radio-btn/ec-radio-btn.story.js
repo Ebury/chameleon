765bac7d541499d79bdd0991a1c639d35ca97cf5
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-12-02 13:43:50 +0100

    [ONL-7047] - dev - reduce stories

--- src/components/ec-radio-btn/ec-radio-btn.story.js
+++ src/components/ec-radio-btn/ec-radio-btn.story.js
@@ -8,7 +8,6 @@ export default {
 };
 
 const basicArgs = {
-  label: 'Select one option',
   options: [
     { value: 'y', label: 'Yes' },
     { value: 'n', label: 'No' },
@@ -19,152 +18,74 @@ export const basic = () => ({
   components: { EcRadioBtn },
   setup() {
     const model = ref('');
-    return { args: basicArgs, model };
-  },
-  template: `
-    <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model"/>
-    </div>
-  `,
-});
-
-export const description = () => ({
-  components: { EcRadioBtn },
-  setup() {
-    const model = ref('');
     return {
-      args: {
-        ...basicArgs,
-        options: [
-          { value: 'y', label: 'Yes', description: 'Confirm option' },
-          { value: 'n', label: 'No', description: 'Reject option' },
-        ],
-      },
+      args: basicArgs,
+      label: 'Select one option',
       model,
     };
   },
   template: `
     <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model" />
+      <ec-radio-btn v-bind="args" :label="label" v-model="model"/>
     </div>
   `,
 });
 
-export const checked = () => ({
+export const all = () => ({
   components: { EcRadioBtn },
   setup() {
-    const model = ref('y');
+    const modelUnchecked = ref('');
+    const modelChecked = ref('y');
+    const optionsWithDescription = [
+      { value: 'y', label: 'Yes', description: 'Confirm' },
+      { value: 'n', label: 'No', description: 'Reject' },
+    ];
+    const errorMessage = 'One of the options must be selected';
+
     return {
-      args: {
-        ...basicArgs,
-      },
-      model,
+      args: basicArgs,
+      modelChecked,
+      modelUnchecked,
+      optionsWithDescription,
+      errorMessage,
     };
   },
   template: `
-    <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model" />
+    <div class="tw-max-w-screen-sm tw-m-24">
+      <h3>Description</h3>
+      <div class="tw-p-24">
+        <ec-radio-btn v-bind="args" v-model="modelUnchecked" :options="optionsWithDescription" />
+      </div>
+
+      <h3>Checked</h3>
+      <div class="tw-p-24">
+        <ec-radio-btn v-bind="args" v-model="modelChecked" />
+      </div>
+
+      <h3>Unchecked Disabled</h3>
+      <div class="tw-p-24">
+        <ec-radio-btn v-bind="args" v-model="modelUnchecked" :disabled="true" />
+      </div>
+
+      <h3>Checked Disabled</h3>
+      <div class="tw-p-24">
+        <ec-radio-btn v-bind="args" v-model="modelChecked" :disabled="true" />
+      </div>
+
+      <h3>Inline Radio Group</h3>
+      <div class="tw-p-24">
+        <ec-radio-btn v-bind="args" v-model="modelChecked" :is-group-inline="true" />
+      </div>
+
+      <h3>Inline Text</h3>
+      <div class="tw-p-24">
+        <ec-radio-btn v-bind="args" v-model="modelChecked" :is-text-inline="true" :options="optionsWithDescription" />
+      </div>
+
+      <h3>Error</h3>
+      <div class="tw-p-24">
+        <ec-radio-btn v-bind="args" v-model="modelUnchecked" :error-message="errorMessage" />
+      </div>
     </div>
   `,
 });
-
-export const checkedDisabled = () => ({
-  components: { EcRadioBtn },
-  setup() {
-    const model = ref('y');
-    return {
-      args: {
-        ...basicArgs,
-        modelValue: 'y',
-        disabled: true,
-      },
-      model,
-    };
-  },
-  template: `
-    <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model" />
-    </div>
-  `,
-});
-
-export const inlineRadioGroup = () => ({
-  components: { EcRadioBtn },
-  setup() {
-    const model = ref('y');
-    return {
-      args: {
-        ...basicArgs,
-        isGroupInline: true,
-      },
-      model,
-    };
-  },
-  template: `
-    <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model" />
-    </div>
-  `,
-});
-
-export const inlineText = () => ({
-  components: { EcRadioBtn },
-  setup() {
-    const model = ref('y');
-    return {
-      args: {
-        ...basicArgs,
-        options: [
-          { value: 'y', label: 'Yes', description: '(Confirm)' },
-          { value: 'n', label: 'No', description: '(Reject)' },
-        ],
-        isTextInline: true,
-      },
-      model,
-    };
-  },
-  template: `
-    <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model" />
-    </div>
-  `,
-});
-
-export const uncheckedDisabled = () => ({
-  components: { EcRadioBtn },
-  setup() {
-    const model = ref('y');
-    return {
-      args: {
-        ...basicArgs,
-        disabled: true,
-      },
-      model,
-    };
-  },
-  template: `
-    <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model" />
-    </div>
-  `,
-});
-
-export const error = () => ({
-  components: { EcRadioBtn },
-  setup() {
-    const model = ref('');
-    return {
-      args: {
-        ...basicArgs,
-        errorMessage: 'One of the options must be selected',
-      },
-      model,
-    };
-  },
-  template: `
-    <div class="tw-p-24">
-      <ec-radio-btn v-bind="args" v-model="model" />
-    </div>
-  `,
-});
-
