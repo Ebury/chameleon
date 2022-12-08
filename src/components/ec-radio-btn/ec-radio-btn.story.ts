7b5d80de645791107fa696bd1485f7286dc7f367
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-12-08 16:21:17 +0100

    [ONL-7047] - ref - updated action

--- src/components/ec-radio-btn/ec-radio-btn.story.ts
+++ src/components/ec-radio-btn/ec-radio-btn.story.ts
@@ -65,7 +65,7 @@ export const all = () => ({
       modelInlineText,
       modelError,
       hasError,
-      onChange: action('change'),
+      onChange: action('update:modelValue'),
     };
   },
   template: `
