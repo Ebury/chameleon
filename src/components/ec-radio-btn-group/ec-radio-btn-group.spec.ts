04d8b624e2a379b151f44ec2c3136281811a2993
Alessio Scaiola <alessio.scaiola@ebury.com>
2022-12-14 16:40:41 +0100

    [ONL-7085] - test:fix - name type

--- src/components/ec-radio-btn-group/ec-radio-btn-group.spec.ts
+++ src/components/ec-radio-btn-group/ec-radio-btn-group.spec.ts
@@ -77,8 +77,8 @@ describe('EcRadioBtn', () => {
 
     it('should have the same given "name" attribute among all the radio inputs', () => {
       const wrapper = mountRadioBtnGroup();
-      const givenNamePropRadio1: string = wrapper.findAllComponents(EcRadioBtn)[0].vm.name;
-      const givenNamePropRadio2: string = wrapper.findAllComponents(EcRadioBtn)[1].vm.name;
+      const givenNamePropRadio1: string | undefined = wrapper.findAllComponents(EcRadioBtn)[0].vm.name;
+      const givenNamePropRadio2: string | undefined = wrapper.findAllComponents(EcRadioBtn)[1].vm.name;
       expect(givenNamePropRadio1).toBe(givenNamePropRadio2);
       expect(givenNamePropRadio1).not.toBe('');
       expect(wrapper.element).toMatchSnapshot();
