1299dac370707341a3e20e0a88174220ed62aa7a
Anton Lazarev <anton.lazarev@ebury.com>
2022-12-19 16:34:58 +0000

    Cypress component tests for EcInputField, icons to do

--- src/components/ec-input-field/ec-input-field.cy.ts
+++ src/components/ec-input-field/ec-input-field.cy.ts
@@ -1,23 +1,218 @@
 /// <reference types="cypress-image-snapshot" />
 import { EcInputField } from '../../main';
 import type { InputFieldProps } from './types';
+import { InputFieldType } from './types';
 
 import 'tailwindcss/dist/tailwind.min.css';
 import '../../styles/main.css';
 import '../../styles/themes/blue.css';
+import type { MountingOptions } from '@vue/test-utils';
+import { defineComponent } from 'vue';
+
+import config from '../../config'
+
+function mountInputField (
+  props?: Partial<InputFieldProps> & { onClick?: unknown, 'onUpdate:modelValue'?: unknown },
+  mountOpts?: MountingOptions<InputFieldProps>) {
+  return cy.mount<InputFieldProps>(
+    EcInputField,
+    {
+      props: {
+        modelValue: 'Text test',
+        type: InputFieldType.TEXT,
+        errorMessage: '',
+        label: 'label test',
+        note: 'note test',
+        labelTooltip: '',
+        ...props,
+      },
+      ...mountOpts,
+    },
+  );
+}
+
+function mountInputFieldAsTemplate (
+  template: string,
+  props?: InputFieldProps,
+  wrapperComponentOpts?: Record<string, unknown>,
+  mountOpts?: MountingOptions<InputFieldProps>,
+) {
+  const Component = defineComponent({
+    components: { EcInputField },
+    template,
+    ...wrapperComponentOpts,
+  });
+debugger;
+  return cy.mount<InputFieldProps, MountingOptions<InputFieldProps>>(Component, {
+    props,
+    ...mountOpts,
+  });
+}
 
 describe('InputField', () => {
-  // eslint-disable-next-line jest/expect-expect
   it('should display properly with the given props', () => {
-    cy.mount<InputFieldProps>(
-      EcInputField,
+    mountInputField({ modelValue: 'Some text' });
+    cy.get('[data-test*=ec-input-field__input]').invoke('attr', 'autocomplete').should('eq', undefined);
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly with the given prop autocomplete OFF', () => {
+    mountInputField({ autocomplete: 'off' });
+    cy.get('[data-test*=ec-input-field__input]').invoke('attr', 'autocomplete').should('eq', 'off');
+  });
+
+  it('renders properly with the given prop autocomplete ON', () => {
+    mountInputField({ autocomplete: 'on' });
+    cy.get('[data-test*=ec-input-field__input]').invoke('attr', 'autocomplete').should('eq', 'on');
+  });
+
+  it('renders properly with the given prop errorMessage', () => {
+    mountInputField({ errorMessage: 'error msg' });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly without the error when the prop errorMessage is given and the isInGroup prop is not empty', () => {
+    mountInputField({ errorMessage: 'error msg', isInGroup: 'right' });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly with the id from the parent', () => {
+    mountInputField({ id: 'id-test' });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly with the errorId from the parent', () => {
+    mountInputField({
+      errorId: 'error-id-test',
+      errorMessage: 'Test error message',
+    });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly without label', () => {
+    mountInputField({ label: '' });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly without note', () => {
+    mountInputField({ note: '' });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders with the attrs min and max', () => {
+    mountInputField({ type: InputFieldType.NUMBER }, {
+      attrs: {
+        min: 5,
+        max: 10,
+      },
+    });
+    cy.get('[data-test*=ec-input-field__input]').invoke('attr', 'min').should('eq', '5');
+    cy.get('[data-test*=ec-input-field__input]').invoke('attr', 'max').should('eq', '10');
+  });
+
+  it('works with the same listeners as the input', () => {
+    const event = cy.spy().as('onClickSpy')
+    mountInputField(
+      {},
       {
-        props: { modelValue: 'Some text' },
+        attrs: {
+          onClick: event,
+        }
       },
     );
 
-    cy.get('[data-test*=ec-input-field__input]').invoke('attr', 'autocomplete').should('eq', undefined);
-    cy.get('[data-test*=ec-input-field__input]').matchImageSnapshot();
+    cy.get('[data-test*=ec-input-field__input]').click();
+    cy.get('@onClickSpy').should('have.been.calledOnce')
+  });
+
+  it('should emit the value when you write on the input', () => {
+    const event = cy.spy().as('onUpdateModelValueSpy')
+    mountInputField(
+      {
+        modelValue: '',
+        'onUpdate:modelValue': event
+      },
+    );
+
+    cy.get('[data-test*=ec-input-field__input]').should('have.value', '');
+    cy.get('[data-test*=ec-input-field__input]').type('some text');
+    cy.get('@onUpdateModelValueSpy').should('have.been.calledWith', 'some text');
+  });
+
+  it('should emit an event when we click on the icon', () => {
+    const event = cy.spy().as('onIconClick')
+    mountInputField({ icon: 'simple-check' }, {
+      attrs: {
+        onIconClick: event
+      }
+    });
+
+    cy.get('[data-test=ec-input-field__icon]').click();
+
+    cy.get('@onIconClick').should('have.been.calledOnce');
+  });
+
+  it('renders properly when disabled', () => {
+    mountInputField({}, { attrs: { disabled: true } });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly when readonly', () => {
+    mountInputField({}, { attrs: { readonly: true } });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly when the labelTooltip prop is set', () => {
+    mountInputField({ labelTooltip: 'Testing the labelTooltip prop' });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('should render with a sensitive class when isSensitive prop is set to true', () => {
+    config.sensitiveClass = 'someSensitive';
+    mountInputField({ isSensitive: true });
+    cy.get('[data-test=ec-input-field__input]').should('have.class', 'someSensitive');
+  });
+
+  it('renders with bottom note', () => {
+    mountInputField({ bottomNote: 'Random bottom note' });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders with warning bottom note', () => {
+    mountInputField({ bottomNote: 'Random bottom note', isWarning: true });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders without bottom note if error message is given', () => {
+    mountInputField({
+      bottomNote: 'Random bottom note',
+      errorMessage: 'Random error message',
+    });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('renders properly custom attributes', () => {
+    mountInputField({}, {
+      attrs: {
+        style: { backgroundColor: 'hotpink' },
+      },
+    });
+    cy.get('[data-test=ec-input-field]').matchImageSnapshot();
+  });
+
+  it('should never assign undefined value to the input', () => {
+    mountInputField({
+      modelValue: undefined,
+    });
+
+    cy.get('[data-test=ec-input-field__input]').should('have.value', '');
+  });
+
+  it('should be focusable from outside', () => {
+    const wrapper = mountInputField();
+
+    cy.get('[data-test=ec-input-field__input]').should('not.be.focused');
+    cy.get('[data-test=ec-input-field__label]').click();
+    cy.get('[data-test=ec-input-field__input]').should('be.focused')
   });
 });
-
