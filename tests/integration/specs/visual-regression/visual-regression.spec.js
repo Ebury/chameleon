4ce77c84889e4e548fc315b87ef7e754b05e2ffd
Alessio Scaiola <alessio.scaiola@ebury.com>
2023-01-30 18:00:01 +0100

    [ONL-7197] - snapshots in the correct folder

--- tests/integration/specs/visual-regression/visual-regression.spec.js
+++ tests/integration/specs/visual-regression/visual-regression.spec.js
@@ -71,7 +71,6 @@ function visitStory(uuid, story, controls) {
     timeout: 120000,
   }, win => new Cypress.Promise(resolve => win.requestIdleCallback(resolve)));
 
-  cy.wait(500);
   if (snapshotElement) {
     cy.get(snapshotElement).matchImageSnapshot(uuid);
   } else {
