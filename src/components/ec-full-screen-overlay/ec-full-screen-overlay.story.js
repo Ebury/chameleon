5366264aebb6a58568826ff55d536dfef43f58fd
Alessio Scaiola <alessio.scaiola@ebury.com>
2023-05-05 13:25:29 +0200

    [ONL-7498] - dev - added stories

--- src/components/ec-full-screen-overlay/ec-full-screen-overlay.story.js
+++ src/components/ec-full-screen-overlay/ec-full-screen-overlay.story.js
@@ -1,6 +1,7 @@
 import { action } from '@storybook/addon-actions';
 
 import { fixedContainerDecorator } from '../../../.storybook/utils';
+import EcBtn from '../ec-btn';
 import EcFullScreenOverlay from './ec-full-screen-overlay.vue';
 
 export default {
@@ -66,3 +67,62 @@ export const withSlots = args => ({
 });
 
 withSlots.args = { ...basic.args };
+
+export const withBackgroundType = Template.bind({});
+
+withBackgroundType.args = {
+  title: 'Lorem title',
+  show: true,
+  backgroundType: '7',
+};
+
+export const noClosingIcon = args => ({
+  components: { EcFullScreenOverlay, EcBtn },
+  setup() {
+    return { args };
+  },
+  template: `
+    <div>
+      <div>Background Text</div>
+
+      <ec-full-screen-overlay
+        v-bind="args"
+        v-on="{ close: onClose }"
+      >
+        <template #header>
+          <div>
+            <div>Lorem logo</div>
+            <h1 class="tw-mt-24">Lorem Title</h1>
+          </div>
+        </template>
+
+        <template #main>
+          <p v-for="i in 10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a tristique enim. Nulla consequat vitae metus in ultricies. Curabitur dapibus, purus quis finibus rhoncus, purus augue blandit neque, quis fringilla urna justo consequat arcu. Nulla facilisi. Sed varius metus tempor, porta nunc in, pulvinar arcu. Duis quis lacus vehicula, lacinia arcu in, cursus tellus.</p>
+          <div class="tw-flex tw-justify-end">
+            <ec-btn
+              class="tw-mr-16"
+              is-rounded
+              :is-submit="false"
+              category="secondary"
+            >
+              Cancel
+            </ec-btn>
+            <ec-btn
+              category="primary"
+              is-rounded
+              :is-submit="false"
+            >
+              Submit
+            </ec-btn>
+          </div>
+        </template>
+      </ec-full-screen-overlay>
+    </div>
+  `,
+});
+
+noClosingIcon.args = {
+  title: 'Lorem title',
+  show: true,
+  showCloseBtn: false,
+};
