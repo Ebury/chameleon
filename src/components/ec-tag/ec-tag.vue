c9bded450357cb396641156a73f4ebeb2cd3f3ff
Stavros Tomas <55188371+stavros-tomas@users.noreply.github.com>
2023-08-08 11:21:31 +0100

    [ONL 7731] feat: new tag component (#1627)
    * ONL-7731:feat:new tag component
    
    * ONL-7731:chore:new integration snapshots
    
    * ONL-7731:chore:extra story
    
    * ONL-7731:feat:extra prop for rounded icon
    
    * 2.2.34

---
+++ src/components/ec-tag/ec-tag.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    v-ec-tooltip="{ content: text }"
+    class="ec-tag"
+    data-test="ec-tag"
+  >
+    <ec-icon
+      v-if="iconName"
+      class="ec-tag__icon"
+      :class="{ 'tw-rounded-1/2': isIconRounded }"
+      data-test="ec-tag__icon"
+      :name="iconName"
+      :type="iconType"
+      :size="16"
+    />
+
+    <div
+      class="ec-tag__text"
+      data-test="ec-tag__text"
+    >{{ text }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import vEcTooltip from '../../directives/ec-tooltip';
+import EcIcon from '../ec-icon';
+import type { IconName, IconType } from '../ec-icon/types';
+
+interface TagProps {
+  text: string,
+  isIconRounded?: boolean,
+  iconName?: IconName,
+  iconType?: IconType,
+}
+
+const props = withDefaults(defineProps<TagProps>(), {
+  isIconRounded: false,
+});
+
+const {
+  iconName, iconType, isIconRounded, text,
+} = toRefs(props);
+</script>
+
+<style>
+.ec-tag {
+  @apply tw-inline-flex tw-items-center;
+  @apply tw-bg-gray-7;
+  @apply tw-py-4 tw-px-8;
+
+  @apply tw-rounded-button;
+
+  &__icon {
+    @apply tw-mr-4;
+    @apply tw-flex-shrink-0;
+  }
+
+  &__text {
+    @apply tw-text-gray-3;
+    @apply tw-truncate;
+  }
+}
+</style>
