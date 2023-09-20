74f8db3ae4056c1f1c3469bd75149ad86859b097
Raul Aguilar <raul.aguilar@ebury.com>
2023-09-20 09:46:11 +0200

    [ONL-7877] use numberOfColumns for colspan

--- src/components/ec-table/ec-table.vue
+++ src/components/ec-table/ec-table.vue
@@ -30,7 +30,7 @@
           >
             <td
               v-if="canShowCustomRow"
-              colspan="100%"
+              :colspan="numberOfColumns"
             >
               <slot
                 :row="row"
