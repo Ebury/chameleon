82ec3eb11ba88df8aad9814eea8910bf9617e182
Milos Cibulka <mcibique@users.noreply.github.com>
2023-11-02 16:39:15 +0000

    [ONL-8065] fix: Fixed CVueWrapper not having correct return values.

--- tests/utils/global.d.ts
+++ tests/utils/global.d.ts
@@ -1,8 +1,15 @@
-import { VueWrapper } from '@vue/test-utils';
+import { DOMWrapper, VueWrapper } from '@vue/test-utils';
 
-export interface CVueWrapper extends VueWrapper {
-  findByDataTest: (string) => CVueWrapper
-  findAllByDataTest: (string) => CVueWrapper[]
-  findComponentByDataTest: (string) => CVueWrapper
-  findAllComponentsByDataTest: (string) => CVueWrapper[]
+export interface CVueWrapper<VM = unknown, T extends ComponentPublicInstance = VM & ComponentPublicInstance> extends VueWrapper<VM, T> {
+  findByDataTest: (string) => CDomWrapper<T>
+  findAllByDataTest: (string) => CDomWrapper<T>[]
+  findComponentByDataTest: (string) => CVueWrapper<T>
+  findAllComponentsByDataTest: (string) => CVueWrapper<T>[]
+}
+
+export interface CDomWrapper<NodeType extends Node> extends DOMWrapper<NodeType> {
+  findByDataTest: (string) => CDomWrapper<NodeType>
+  findAllByDataTest: (string) => CDomWrapper<NodeType>[]
+  findComponentByDataTest: (string) => CVueWrapper<NodeType>
+  findAllComponentsByDataTest: (string) => CVueWrapper<NodeType>[]
 }
