f7dd3fcf30a55e6070aa5fbb93a76f7fbc15a66d
Milos Cibulka <mcibique@users.noreply.github.com>
2023-11-07 13:19:40 +0000

    [ONL-8065] chore: Removed unused generic type.

--- tests/utils/global.d.ts
+++ tests/utils/global.d.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { DOMWrapper, VueWrapper } from '@vue/test-utils';
 
 declare module '@vue/test-utils' {
@@ -8,7 +6,7 @@ declare module '@vue/test-utils' {
     findByDataTest<T extends Node = Node>(selector: string | RefSelector): DOMWrapper<T>;
     findAllByDataTest<T extends Element>(selector: string): DOMWrapper<T>[];
     findComponentByDataTest<T extends Element>(selector: string): VueWrapper<T>;
-    findComponentByDataTest<T extends never>(selector: string): WrapperLike;
+    findComponentByDataTest(selector: string): WrapperLike;
     findAllComponentsByDataTest<T extends Element>(selector: string): VueWrapper<T>[];
   }
 }
