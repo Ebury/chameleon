b55ee2d1df6d003ad760702dc1d5059b6efd1918
Milos Cibulka <mcibique@users.noreply.github.com>
2023-11-22 13:17:13 +0000

    wip

--- src/directives/testing.spec.js
+++ src/directives/testing.spec.js
@@ -2,4 +2,8 @@ describe('Test', () => {
   it('should not fail', () => {
     expect(1 + 1).toMatchSnapshot();
   });
+
+  it('should not fail 2', () => {
+    expect(1 + 1).toMatchSnapshot();
+  });
 });
