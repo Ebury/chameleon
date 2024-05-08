2245f6571daa1ff4270919f7b24b8c7c848081cc
Raúl Aguilar <raul.aguilar@ebury.com>
2024-05-08 11:44:38 +0200

    [CHUX-457] Create letter-icon component (#1945)
    * [CHUX-457] add component and story
    
    * [CHUX-457] add unit tests and snapshot
    
    * [CHUX-457] add image snapshots
    
    * [CHUX-457] change text color on hover
    
    * [CHUX-457] design feedback
    
    * 2.8.4
    
    * [CHUX-457] revert version bump
    
    * [CHUX-457] use button element if icon is clickable
    
    * [CHUX-457] always display uppercase letter
    
    * [CHUX-457] replace svg
    
    * [CHUX-457] coverage
    
    * [CHUX-457] remove leftover styles
    
    * [CHUX-457] computed refactor
    
    * [CHUX-457] size enum
    
    * [CHUX-457] hide icon if text prop is empty

---
+++ src/components/ec-letter-icon/ec-letter-icon.spec.ts
@@ -0,0 +1,73 @@
+import { type ComponentMountingOptions, mount } from '@vue/test-utils';
+
+import EcLetterIcon from './ec-letter-icon.vue';
+import { type LetterIconProps, LetterIconSize } from './types';
+
+describe('EcLetterIcon', () => {
+  function mountLetterIcon(props: LetterIconProps, mountOpts?: ComponentMountingOptions<typeof EcLetterIcon>) {
+    return mount(
+      EcLetterIcon,
+      {
+        props: {
+          ...props,
+        },
+        ...mountOpts,
+      },
+    );
+  }
+
+  it('should render as expected', () => {
+    const wrapper = mountLetterIcon({
+      text: 'Test',
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should render the first letter of the text we passed through prop', () => {
+    const wrapper = mountLetterIcon({
+      text: 'Test',
+    });
+
+    const text = wrapper.findByDataTest('ec-letter-icon__text');
+    expect(text.text()).toBe('T');
+  });
+
+  it('should render the letter as uppercase if the text begins with a lowercase character', () => {
+    const wrapper = mountLetterIcon({
+      text: 'test',
+    });
+
+    const text = wrapper.findByDataTest('ec-letter-icon__text');
+    expect(text.text()).toBe('T');
+  });
+
+  it('should not display the icon if text is an empty string', () => {
+    const wrapper = mountLetterIcon({
+      text: '',
+    });
+
+    const container = wrapper.findByDataTest('ec-letter-icon');
+    expect(container.exists()).toBe(false);
+  });
+
+  it('should apply corresponding class if "size" prop is set', () => {
+    const wrapper = mountLetterIcon({
+      text: 'Test',
+      size: LetterIconSize.MEDIUM,
+    });
+
+    const container = wrapper.findByDataTest('ec-letter-icon');
+    expect(container.classes()).toContain('ec-letter-icon--md');
+  });
+
+  it('should use button element and add clickable class if "isClickable" prop is set', () => {
+    const wrapper = mountLetterIcon({
+      text: 'Test',
+      isClickable: true,
+    });
+
+    const container = wrapper.findByDataTest('ec-letter-icon');
+    expect(container.element.tagName).toContain('BUTTON');
+    expect(container.classes()).toContain('ec-letter-icon--clickable');
+  });
+});
---
+++ src/components/ec-letter-icon/types.ts
@@ -0,0 +1,10 @@
+export interface LetterIconProps {
+  text: string,
+  size?: LetterIconSize,
+  isClickable?: boolean,
+}
+
+export enum LetterIconSize {
+  SMALL = 'sm',
+  MEDIUM = 'md',
+}
