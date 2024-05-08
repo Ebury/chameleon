0d60d792038f95b8aa881e81ef0976768e2f0726
Raúl Aguilar <raul.aguilar@ebury.com>
2024-05-08 11:51:40 +0200

    [CHUX-460] Add new prop to ec-container (#1947)
    * [CHUX-460] add showMobileLayout prop
    
    * [CHUX-460] rename prop to isResponsive and set default to true
    
    * [CHUX-460] remove default true
    
    * [CHUX-460] rename story to responsive layout

--- src/components/ec-container/ec-container.vue
+++ src/components/ec-container/ec-container.vue
@@ -1,11 +1,15 @@
 <template>
   <div
     class="ec-container"
+    :class="{ 'ec-container--is-responsive': isResponsive }"
     data-test="ec-container"
   >
     <div
       class="ec-container__navigation"
-      :class="{ 'ec-container__navigation--is-collapsable': isCollapsable }"
+      :class="{
+        'ec-container__navigation--is-collapsable': isCollapsable,
+        'ec-container__navigation--is-responsive': isResponsive,
+      }"
       data-test="ec-container__navigation"
     >
       <slot name="navigation" />
@@ -30,11 +34,19 @@ defineProps<ContainerProps>();
   @apply tw-flex tw-flex-row tw-items-stretch;
   @apply tw-min-h-screen;
 
+  &--is-responsive {
+    @apply tw-flex-col;
+  }
+
   &__navigation {
     flex-basis: 280px;
 
     &--is-collapsable {
       flex-basis: 80px;
+    }
+
+    &--is-responsive {
+      @apply tw-basis-auto;
     }
   }
 
