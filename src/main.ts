8c5c1ae2189fac15271c037a98fc3d520485510e
Raul Aguilar Garcia <raul.aguilar@ebury.com>
2024-05-16 16:39:57 +0200

    [CHUX-465] add exports in main.ts

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,8 @@ export { default as EcMetrolineItem } from './components/ec-metroline/components
 export * from './components/ec-metroline/components/ec-metroline-item/types';
 export { METROLINE_PROVIDE_KEY } from './components/ec-metroline/provide';
 export * from './components/ec-metroline/types';
+export { default as EcMobileHeader } from './components/ec-mobile-header';
+export * from './components/ec-mobile-header/types';
 export { default as EcModal } from './components/ec-modal';
 export * from './components/ec-modal/types';
 export { default as EcMultipleValuesSelection } from './components/ec-multiple-values-selection';
