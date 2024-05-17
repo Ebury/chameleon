26c42fc228b11c0f28dddecc2af00ef37583d289
Raul Aguilar Garcia <raul.aguilar@ebury.com>
2024-05-17 10:32:32 +0200

    [CHUX-465] add responsive story in navigation

--- src/components/ec-navigation/ec-navigation.story.ts
+++ src/components/ec-navigation/ec-navigation.story.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryFn } from '@storybook/vue3';
+import { useMediaQuery } from '@vueuse/core';
 import { ref, watchEffect } from 'vue';
 
 import { fixedContainerDecorator } from '../../../.storybook/utils';
@@ -79,6 +80,82 @@ basic.args = {
     logo: '/ebury-logo-sm-inverse.png',
   },
   isCollapsable: true,
+  isCollapsed: false,
+  showBrandingLogo: true,
+  showCopyright: true,
+};
+
+export const responsive: StoryFn<NavigationProps & {
+  showCopyright: boolean,
+}> = storyArgs => ({
+  components: { EcNavigation },
+  setup() {
+    const showCopyright = ref(false);
+    const isCollapsable = ref(false);
+    const isCollapsed = ref(false);
+    const isResponsive = ref(false);
+    const args = ref({});
+
+    watchEffect(() => {
+      const {
+        showCopyright: showCopyrightFromArgs,
+        isCollapsable: isCollapsableFromArgs,
+        isCollapsed: isCollapsedFromArgs,
+        isResponsive: isResponsiveFromArgs,
+        ...rest
+      } = storyArgs;
+      showCopyright.value = showCopyrightFromArgs;
+      isCollapsable.value = isCollapsableFromArgs;
+      isCollapsed.value = isCollapsedFromArgs;
+      isResponsive.value = isResponsiveFromArgs || useMediaQuery('(max-width: 1279px)').value;
+      args.value = rest;
+    });
+
+    return {
+      showCopyright,
+      isCollapsable,
+      isCollapsed,
+      isResponsive,
+      args,
+      styles: ref({
+        placeholder: {
+          backgroundColor: 'hsla(var(--ec-gray-color-level-8), .1)',
+        },
+      }),
+    };
+  },
+  template: `
+    <ec-navigation
+      v-bind="args"
+      :is-collapsable="isCollapsable"
+      :is-collapsed="isCollapsable && isCollapsed"
+      :is-responsive="isResponsive"
+    >
+      <template #user-info>
+        <div :style="styles.placeholder" class="tw-p-8 tw-text-center tw-h-full tw-break-words">User Info placeholder</div>
+      </template>
+      <template #call-to-action>
+        <div :style="styles.placeholder" class="tw-p-8 tw-text-center tw-h-full tw-break-words">CTA placeholder</div>
+      </template>
+      <template #menu>
+        <div :style="styles.placeholder" class="tw-p-8 tw-text-center tw-h-full tw-break-words">Menu placeholder</div>
+      </template>
+      <template #footer-menu>
+        <div :style="styles.placeholder" class="tw-p-8 tw-text-center tw-h-full tw-break-words">Footer Menu placeholder</div>
+      </template>
+      <template #copyright v-if="showCopyright">
+        <div :style="styles.placeholder" class="tw-p-8 tw-text-center tw-h-full tw-break-words">Copyright placeholder</div>
+      </template>
+    </ec-navigation>
+  `,
+});
+
+responsive.args = {
+  branding: {
+    name: 'My Branding',
+    logo: '/ebury-logo-sm.png',
+  },
+  isCollapsable: false,
   isCollapsed: false,
   showBrandingLogo: true,
   showCopyright: true,
